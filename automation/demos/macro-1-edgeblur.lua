ef6ef4c603c9b481c6f34882fbc175b47be62c79
Niels Martin Hansen <nielsm@aegisub.org>
2006-12-28 21:19:59 +0000

    More auto4 files...
    Originally committed to SVN as r645.

---
+++ automation/demos/macro-1-edgeblur.lua
@@ -0,0 +1,19 @@
+-- Automation 4 demo script
+-- Macro that adds \be1 tags in front of every selected line
+
+script_name = "Add edgeblur macro"
+script_description = "A testmacro showing how to do simple line modification in Automation 4"
+script_author = "Niels Martin Hansen"
+script_version = "1"
+
+
+function add_edgeblur(subtitles, selected_lines, active_line)
+	for z, i in ipairs(selected_lines) do
+		local l = subtitles[i]
+		l.text = "{\\be1}" .. l.text
+		subtitles[i] = l
+	end
+	aegisub.set_undo_point("Add edgeblur")
+end
+
+aegisub.register_macro("Add edgeblur", "Adds \be1 tags to all selected lines", "edit", add_edgeblur)
