2e1263055ba88d357e4e14ea5a8968fc326c764b
Rodrigo Braz Monteiro <zeratul@cellosoft.com>
2007-04-10 16:16:31 +0000

    Moved stdint.h to win32 to avoid issues with Linux, and removed video_zoom.cpp from Linux makefile.
    Originally committed to SVN as r1056.

--- aegisub/win32/stdint.h
+++ aegisub/win32/stdint.h
@@ -29,9 +29,7 @@
 // 
 ///////////////////////////////////////////////////////////////////////////////
 
-#ifndef _MSC_VER // [
-#error "Use this header only with Microsoft Visual C++ compilers!"
-#endif // _MSC_VER ]
+#ifdef _MSC_VER
 
 #ifndef _MSC_STDINT_H_ // [
 #define _MSC_STDINT_H_
@@ -220,3 +218,4 @@ typedef uint64_t  uintmax_t;
 
 
 #endif // _MSC_STDINT_H_ ]
+#endif // _MSC_VER ]
