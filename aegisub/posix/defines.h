7f6c3a4ba1ddaac8f4576deeacf8169e9a0504d4
Rodrigo Braz Monteiro <zeratul@cellosoft.com>
2007-04-22 07:04:31 +0000

    1111 GET - failed attempt to get Ruby to work (commented out) and made PortAudio optional (I hope)
    Originally committed to SVN as r1111.

--- aegisub/posix/defines.h
+++ aegisub/posix/defines.h
@@ -22,35 +22,22 @@ typedef int64_t __int64;
 typedef uint64_t __uint64;
 #define abs64 llabs
 
-#if defined(HAVE_LIBAVCODEC) && defined(HAVE_LIBAVFORMAT)
-#define USE_LAVC 1
-#endif
+/*
 #if defined(HAVE_ASA) && defined(HAVE_ASA_H)
 #define USE_ASA 1
 #endif
 #if defined(HAVE_LIBSSA) && defined(HAVE_LIBSSA_LIBSSA_H)
 #define USE_LIBSSA 1
 #endif
-#ifdef HAVE_LIBPORTAUDIO
-#define USE_PORTAUDIO 1
-#else
-#error no audio system available
-#endif
 
-//#define USE_HUNSPELL 0
-#ifndef USE_LAVC
-#define USE_LAVC 0
-#endif
 #define USE_PRS 0
 #ifndef USE_FEXTRACKER
-#define USE_FEXTRACKER 1
-#endif
-#ifndef USE_LIBSSA
-#define USE_LIBSSA 0
+#define USE_FEXTRACKER 0
 #endif
 #ifndef USE_ASA
 #define USE_ASA 0
 #endif
+*/
 
 #include "res.h"
 
