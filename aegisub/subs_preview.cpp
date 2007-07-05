4126b0e5bcc830d43a3b2c28372568ee081f24c0
David Lamparter <equinox@diac24.net>
2007-07-05 19:03:26 +0000

    fix #398 (crash on opening style editor)
    Originally committed to SVN as r1379.

--- aegisub/subs_preview.cpp
+++ aegisub/subs_preview.cpp
@@ -155,7 +155,7 @@ void SubtitlesPreview::UpdateBitmap(int w,int h) {
 		subs->LoadDefault();
 		int ver = 1;
 		wxString outGroup;
-		subs->InsertStyle(style);
+		subs->InsertStyle((AssStyle *)style->Clone());
 		subs->SetScriptInfo(_T("PlayResX"),wxString::Format(_T("%i"),w));
 		subs->SetScriptInfo(_T("PlayResY"),wxString::Format(_T("%i"),h));
 		subs->AddLine(_T("Dialogue: 0,0:00:00.00,0:00:05.00,Preview,,0000,0000,0000,,{\\q2}") + showText,_T("[Events]"),0,ver,&outGroup);
