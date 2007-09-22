808f56d294208260297a3367c1a47a160339c55d
Niels Martin Hansen <nielsm@aegisub.org>
2007-09-22 03:50:05 +0000

    Fix heights of search for/replace with fields
    Originally committed to SVN as r1578.

--- aegisub/dialog_search_replace.cpp
+++ aegisub/dialog_search_replace.cpp
@@ -61,14 +61,14 @@ DialogSearchReplace::DialogSearchReplace (wxWindow *parent,bool _hasReplace,wxSt
 	// Find sizer
 	wxSizer *FindSizer = new wxFlexGridSizer(2,2,5,15);
 	wxArrayString FindHistory = Options.GetRecentList(_T("Recent find"));
-	FindEdit = new wxComboBox(this,-1,_T(""),wxDefaultPosition,wxSize(300,20),FindHistory,wxCB_DROPDOWN);
+	FindEdit = new wxComboBox(this,-1,_T(""),wxDefaultPosition,wxSize(300,-1),FindHistory,wxCB_DROPDOWN);
 	//if (FindHistory.Count()) FindEdit->SetStringSelection(FindHistory[0]);
 	FindEdit->SetSelection(0);
 	FindSizer->Add(new wxStaticText(this,-1,_("Find what:")),0,wxRIGHT | wxALIGN_CENTER_VERTICAL,0);
 	FindSizer->Add(FindEdit,0,wxRIGHT,0);
 	if (hasReplace) {
 		wxArrayString ReplaceHistory = Options.GetRecentList(_T("Recent replace"));
-		ReplaceEdit = new wxComboBox(this,-1,_T(""),wxDefaultPosition,wxSize(300,20),ReplaceHistory,wxCB_DROPDOWN);
+		ReplaceEdit = new wxComboBox(this,-1,_T(""),wxDefaultPosition,wxSize(300,-1),ReplaceHistory,wxCB_DROPDOWN);
 		FindSizer->Add(new wxStaticText(this,-1,_("Replace with:")),0,wxRIGHT | wxALIGN_CENTER_VERTICAL,0);
 		FindSizer->Add(ReplaceEdit,0,wxRIGHT,0);
 		ReplaceEdit->SetSelection(0);
