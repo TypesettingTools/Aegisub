c4347d9e8a52f790020d1d2b1a28ab16a69bf810
Niels Martin Hansen <nielsm@aegisub.org>
2007-10-19 03:34:07 +0000

    A bunch more tips for the tip of the day window, especially focused on audio timing.
    Originally committed to SVN as r1616.

--- aegisub/dialog_tip.cpp
+++ aegisub/dialog_tip.cpp
@@ -47,17 +47,21 @@
 // Constructor
 TipOfTheDay::TipOfTheDay (size_t currentTip): wxTipProvider(currentTip) {
 	curTip = currentTip;
-	tips.push_back(_("Aegisub can export subtitles to other formats, character encodings, and even compensate Variable Frame Rate so you can hardsub them - it's all in the Export option in File menu."));
+	tips.push_back(_("Aegisub can export subtitles to many different formats, character encodings, and even compensate Variable Frame Rate so you can hardsub them - it's all in the Export option in File menu."));
 	tips.push_back(_("You can easily translate subtitle files using the translation assistant."));
 	tips.push_back(_("Styles can be stored in different storages, so that you can keep your projects organized."));
 	tips.push_back(_("Use keyboard shortcuts! They make your life easier, for example, Ctrl+Enter updates changes on current line without going to next. Check the manual for a complete list."));
-	tips.push_back(_("There is no reason to use the SSA format (as opposed to ASS). ASS is very similar, but adds some important functionality. Most importantly, however, is that only ASS supports certain override tags (such as \\pos and \\t). Those only work on SSA files because VSFilter/Textsub are merciful."));
+	tips.push_back(_("There is no reason to use the SSA format (as opposed to ASS). ASS is very similar, but adds some important functionality. Most importantly, however, is that only ASS supports certain override tags (such as \\pos and \\t). Those only work on SSA files because VSFilter/Textsub is merciful."));
 	tips.push_back(_("DON'T PANIC!"));
 	tips.push_back(_("Aegisub has several features to make sure you will never lose your work. It will periodically save your subtitles to autosave folder, and will create a copy of subs whenever you open them, to autoback folder. Also, if it crashes, it will attempt to save a restore file."));
 	tips.push_back(_("The styling assistant is a practical way to set styles to each line, when each actor has a different style assigned to it."));
 	tips.push_back(_("The fonts collector is one of the most useful features, which resumes the boring task of hunting down fonts into a matter of a few clicks."));
 	tips.push_back(_("When you are done with your subtitles and ready to distribute them, remember: say no to MP4, OGM or AVI. Matroska is your friend."));
 	tips.push_back(_("Much like anything loaded via DirectShow, certain files may have a strange structure (such as h.264 into AVI or XviD will null frames) which may cause unreliable seeking (that is, video frames might be off by one frame). This is not an Aegisub bug - you may consider reencoding those videos before working with them."));
+	tips.push_back(_("Try the spectrum mode for the audio display, it can make it much easier to spot where the important points in the audio are."));
+	tips.push_back(_("If you decode your audio to a PCM WAV file before loading it in Aegisub, you don't have to wait for it to be decoded before you can use it."));
+	tips.push_back(_("Having video open is often more a nuisance than a help when timing subtitles. Timing with only audio open is often much easier. You can always adjust the subtitles to match the video later on."));
+	tips.push_back(_("If the audio doesn't seem to work properly during video playback, try loading audio separately. Just select Audio->Load from video, that usually makes the audio much more reliable."));
 	tips.push_back(_("If anything goes wrong, blame movax."));
 	//tips.push_back(_(""));
 }
