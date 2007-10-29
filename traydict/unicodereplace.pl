c22c36c687198098efa5e3f1ab732b27436dec34
Karl Blomster <kalle.blomster@gmail.com>
2007-10-29 15:49:46 +0000

    fixed tabs, argh
    Originally committed to SVN as r1638.

--- traydict/unicodereplace.pl
+++ traydict/unicodereplace.pl
@@ -17,13 +17,13 @@ open(OUTFILE, ">:utf8", $outfile) or die("Can't open $outfile: $!");
 
 # loop over lines in the infile
 while (<INFILE>) {
-	for (split('', $_)) { 									# loop over characters in the line
+	for (split('', $_)) { # loop over characters in the line
 		my $cp = ord;
-		if ($cp < 127 or $cp == 0xFFFE or $cp == 0xFEFF) {  # is it a normal ASCII codepoint or a BOM?
-			print OUTFILE $_;								# then pass through unchanged
+		if ($cp < 127 or $cp == 0xFFFE or $cp == 0xFEFF) { # is it a normal ASCII codepoint or a BOM?
+			print OUTFILE $_; # then pass through unchanged
 		} 
 		else {
-			print OUTFILE "\\u", sprintf("%04x", $cp);  	# otherwise print as \uHEX
+			print OUTFILE "\\u", sprintf("%04x", $cp); # otherwise print as \uHEX
 		}
 	}
 }
