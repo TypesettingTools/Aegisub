86271e82b7bd0d89bd5aba6c67472c0e83354701
Rodrigo Braz Monteiro <zeratul@cellosoft.com>
2008-01-15 04:32:48 +0000

    Added a make_pot.bat to generate translation template, and updated aegisub.pot.
    Originally committed to SVN as r1728.

---
+++ po/make_pot.bat
@@ -0,0 +1,4 @@
+del /s list.txt
+dir /w /b ..\aegisub\*.cpp ..\aegisub\*.h >> list.txt
+"c:\program files\gettext\bin\xgettext.exe" --files-from=list.txt --directory=../aegisub/ --output=aegisub.pot --c++ -k_
+pause
