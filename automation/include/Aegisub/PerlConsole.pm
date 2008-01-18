09924e97165065ffb50a9e049cc45c334b686d75
Karl Blomster <kalle.blomster@gmail.com>
2008-01-18 01:46:12 +0000

    Added auto4perl includes and (highly incomplete) API docs
    Originally committed to SVN as r1758.

---
+++ automation/include/Aegisub/PerlConsole.pm
@@ -0,0 +1,6 @@
+package Aegisub::PerlConsole;
+use Exporter 'import';
+
+@EXPORT = qw( echo register_console );
+
+1;
---
+++ automation/include/Aegisub/Script.pm
@@ -0,0 +1,6 @@
+package Aegisub::Script;
+use Exporter 'import';
+
+@EXPORT = qw( register_macro set_info );
+
+1;
