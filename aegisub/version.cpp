d40168007f1c79d40530c6f3f3331f0f0526f706
Rodrigo Braz Monteiro <zeratul@cellosoft.com>
2008-03-07 04:21:26 +0000

    Updated version.cpp for the new release.
    Originally committed to SVN as r1951.

--- aegisub/version.cpp
+++ aegisub/version.cpp
@@ -78,7 +78,7 @@ struct VersionInfoStruct {
 		wxString SCMStr, VersionStr;
 
 		// Update this whenever a new version is released
-		VersionNumber = _T("v2.1.0");
+		VersionNumber = _T("v2.1.1");
 #ifdef _DEBUG
 		IsDebug = true;
 #else
