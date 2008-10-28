ff90f0ea1836f8d3a038fd41f39da5e397f62a76
Rodrigo Braz Monteiro <zeratul@cellosoft.com>
2008-10-28 00:46:16 +0000

    Updated translation template and pt_BR.
    Originally committed to SVN as r2421.

--- po/make_pot.bat
+++ po/make_pot.bat
@@ -1,3 +1,4 @@
 del /s list.txt
 dir /w /b ..\aegisub\*.cpp ..\aegisub\*.h >> list.txt
-"c:\program files\gettext\bin\xgettext.exe" --files-from=list.txt --directory=../aegisub/ --output=aegisub.pot --c++ -k_
+"C:\Program Files (x86)\GnuWin32\bin\xgettext.exe" --files-from=list.txt --directory=../aegisub/ --output=aegisub.pot --c++ -k_
+pause
