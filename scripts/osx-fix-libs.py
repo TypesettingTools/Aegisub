b4175fbd7506803e44e3556dd608f6caa7573d09
Amar Takhar <verm@aegisub.org>
2008-12-29 01:13:55 +0000

    * Copy make-app-bundle.sh to osx-fix-libs.py which is the python script. * Rename make-app-bundle.sh to osx-bundle.sh which is the sh script.
    Originally committed to SVN as r2541.

--- scripts/osx-fix-libs.py
+++ scripts/osx-fix-libs.py
@@ -1,20 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env python
 
-test -f aegisub/aegisub && test -x aegisub/aegisub || ( exit "Make sure you're in the right dir"; exit 1 )
-test -e $1 && ( echo "$1 already exists, will not overwrite."; exit 1 )
-
-echo "Making directory structure..."
-mkdir $1 || ( echo "Failed creating directory $1"; exit 1 )
-mkdir $1/Contents $1/Contents/MacOS $1/Contents/Resources
-
-echo "Copying files into package..."
-cp aegisub/macosx/Info.plist $1/Contents
-cp aegisub/aegisub $1/Contents/MacOS
-cp aegisub/macosx/*.icns $1/Contents/Resources
-
-echo "Now about to collect and fix up shared libraries..."
-
-python - $1/Contents/MacOS/aegisub <<FIXLIBS_END
 import re
 import sys
 import os
@@ -66,7 +51,3 @@ for lib in libs:
 	sys.stdout.flush()
 print
 print "All done!"
-FIXLIBS_END
-
-echo "Done creating $1!"
-
