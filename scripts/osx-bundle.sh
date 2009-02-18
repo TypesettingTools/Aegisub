c4d4e6db988fe55b45ec1c03c7cb1dc68e3c9286
Amar Takhar <verm@aegisub.org>
2009-02-18 06:00:17 +0000

    Use $DICT_DIR to find diciontary files for copying inside the bundle. The default is ~/dict.
    Originally committed to SVN as r2741.

--- scripts/osx-bundle.sh
+++ scripts/osx-bundle.sh
@@ -4,6 +4,8 @@ PKG_DIR=${1}.app
 SKEL_DIR="packages/osx_bundle"
 AEGISUB_VERSION_DATA="${2}"
 SRCDIR=`pwd`
+HOME_DIR=`echo ~`
+
 if test -z "${CC}"; then
   CC="cc"
 fi
@@ -38,6 +40,21 @@ cp ${SKEL_DIR}/Contents/Resources/etc/fonts/fonts.dtd ${PKG_DIR}/Contents/Resour
 cat ${SKEL_DIR}/Contents/Resources/etc/fonts/fonts.conf |sed -f scripts/osx-bundle.sed > ${PKG_DIR}/Contents/Resources/etc/fonts/fonts.conf
 cp ${SKEL_DIR}/Contents/Resources/etc/fonts/conf.d/*.conf ${PKG_DIR}/Contents/Resources/etc/fonts/conf.d
 cat ${SKEL_DIR}/Contents/Info.plist |sed -f scripts/osx-bundle.sed > ${PKG_DIR}/Contents/Info.plist
+
+
+echo
+echo "---- Copying dictionaries ----"
+if test -z ${DICT_DIR}; then
+  DICT_DIR="${HOME_DIR}/dict"
+fi
+
+if test -d "${DICT_DIR}"; then
+  cp -v ${DICT_DIR}/* ${PKG_DIR}/Contents/SharedSupport/dictionaries
+else
+  echo "WARNING: Dictionaries not found, please set $$DICT_DIR to a directiory"
+  echo "         where the *.aff and *.dic files can be found"
+fi
+
 
 echo
 echo "---- Copying automation/ files ----"
