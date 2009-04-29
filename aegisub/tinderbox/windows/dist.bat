29721b983e979f8855040c7a21499bf92ef0cfe4
Amar Takhar <verm@aegisub.org>
2009-04-29 08:07:22 +0000

    s/32/??/ to include 64bit builds (what was I thinking?).. it's better than using '*' which could lead to a disaster.
    Originally committed to SVN as r2877.

--- aegisub/tinderbox/windows/dist.bat
+++ aegisub/tinderbox/windows/dist.bat
@@ -1,8 +1,8 @@
 set dist_dir="aegisub-%1-%2-%3-r%4"
 md %dist_dir%
-xcopy aegisub\tinderbox\windows\bin\aegisub32.exe %dist_dir%
-xcopy aegisub\tinderbox\windows\bin\aegisub32.pdb %dist_dir%
-xcopy aegisub\tinderbox\windows\bin\libauto3_32.dll %dist_dir%
-xcopy aegisub\tinderbox\windows\bin\libauto3_32.pdb %dist_dir%
+xcopy aegisub\tinderbox\windows\bin\aegisub??.exe %dist_dir%
+xcopy aegisub\tinderbox\windows\bin\aegisub??.pdb %dist_dir%
+xcopy aegisub\tinderbox\windows\bin\libauto3_??.dll %dist_dir%
+xcopy aegisub\tinderbox\windows\bin\libauto3_??.pdb %dist_dir%
 
 "c:\Program Files (x86)\7-Zip\7z.exe" -bd a dist.7z %dist_dir%/*
