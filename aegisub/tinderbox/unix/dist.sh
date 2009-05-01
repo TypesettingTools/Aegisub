35cea05304616e49795c2d4d2e5698f53794d601
Amar Takhar <verm@aegisub.org>
2009-05-01 03:58:23 +0000

    Add an sh script to generate snap tarballs upon successful builds of UNIX platforms (excluding Darwin).
    Originally committed to SVN as r2889.

---
+++ aegisub/tinderbox/unix/dist.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+DIST_NAME="aegisub-snap-r${1}"
+
+if test -z "${1}"; then
+  echo "You must supply a revision number!"
+  exit 1
+fi
+
+# On FreeBSD "make" is PMake, so we need to use 'gmake'
+if test `uname -s` = "FreeBSD"; then
+  BIN_MAKE="gmake"
+else
+  BIN_MAKE="make"
+fi
+
+./autogen.sh || exit $?
+
+${BIN_MAKE} distdir distdir="${DIST_NAME}" || exit $?
+
+# Put the name of the buildslave into ./slave_info as it may be useful for
+# debugging localised buildslave issues if a user is using a snapshot.
+echo "${2}" > "${DIST_NAME}/slave_info"
+
+tar cf "${DIST_NAME}.tar" "${DIST_NAME}" || exit $?
+bzip2 -v ${DIST_NAME}.tar
+mv "${DIST_NAME}.tar.bz2" dist.tar.bz2
+
