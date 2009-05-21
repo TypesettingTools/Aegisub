68d26811937f199019c7c64ce8391d0c5c6333ff
harukalover <harukalover@aegisub.org>
2009-05-21 19:49:23 +0000

    Fixed a memory leak in the dummy video provider detected by valgrind. The video frame was being allocated but never deallocated.
    Originally committed to SVN as r2962.

--- aegisub/src/video_provider_dummy.cpp
+++ aegisub/src/video_provider_dummy.cpp
@@ -194,6 +194,7 @@ DummyVideoProvider::DummyVideoProvider(double _fps, int frames, int _width, int 
 //////////////
 // Destructor
 DummyVideoProvider::~DummyVideoProvider() {
+	frame.Clear();
 }
 
 
