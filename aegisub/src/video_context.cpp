TypesettingTools/Aegisub

commit 49e6d54006b3fb3ace5ee3cdc23b472a4e4700c0

Add some error catching in the OpenGL code, this should make most OpenGL-related problems less fatal, though it can still cause funky UI problems I think. At least the user should get a slightly more useful error message now. Updates #799.
git-svn-id: svn+ssh://aegisub/var/svn/aegisub/trunk@2998 93549f3f-7f0a-0410-a4b3-e966c9c94f04

aegisub/src/video_context.cpp
@@ -424,15 +424,32 @@ void VideoContext::JumpToFrame(int n) {
 
 	// Not threaded
 	else {
-		// Set frame number
-		frame_n = n;
-		GetFrameAsTexture(n);
+		try {
+			// Set frame number
+			frame_n = n;
+			GetFrameAsTexture(n);
 
-		// Display
-		UpdateDisplays(false);
+			// Display
+			UpdateDisplays(false);
 
-		// Update grid
-		if (!isPlaying && Options.AsBool(_T("Highlight subs in frame"))) grid->Refresh(false);
+			// Update grid
+			if (!isPlaying && Options.AsBool(_T("Highlight subs in frame"))) grid->Refresh(false);
+		}
+		catch (const wxChar *err) {
+			wxLogError(
+				_T("Failed seeking video. The video will be closed because of this.\n")
+				_T("If you get this error regardless of which video file you use, and also if you use dummy video, your OpenGL driver might not work with Aegisub.\n")
+				_T("Error message reported: %s"),
+				err);
+			Reset();
+		}
+		catch (...) {
+			wxLogError(
+				_T("Failed seeking video. The video will be closed because of this.\n")
+				_T("If you get this error regardless of which video file you use, and also if you use dummy video, your OpenGL driver might not work with Aegisub.\n")
+				_T("No further error message given."));
+			Reset();
+		}
 	}
 }
 

aegisub/src/video_display.cpp
@@ -168,7 +168,10 @@ void VideoDisplay::ShowCursor(bool show) {
 
 //////////
 // Render
-void VideoDisplay::Render() {
+void VideoDisplay::Render()
+// Yes it's legal C++ to replace the body of a function with one huge try..catch statement
+try {
+
 	// Is shown?
 	if (!IsShownOnScreen()) return;
 	if (!wxIsMainThread()) throw _T("Error: trying to render from non-primary thread");
@@ -306,6 +309,21 @@ void VideoDisplay::Render() {
 	glFinish();
 	//if (glGetError()) throw _T("Error finishing gl operation.");
 	SwapBuffers();
+}
+catch (const wxChar *err) {
+	wxLogError(
+		_T("An error occurred trying to render the video frame to screen.\n")
+		_T("If you get this error regardless of which video file you use, and also if you use dummy video, your OpenGL driver might not work with Aegisub.\n")
+		_T("Error message reported: %s"),
+		err);
+	VideoContext::Get()->Reset();
+}
+catch (...) {
+	wxLogError(
+		_T("An error occurred trying to render the video frame to screen.\n")
+		_T("If you get this error regardless of which video file you use, and also if you use dummy video, your OpenGL driver might not work with Aegisub.\n")
+		_T("No further error message given."));
+	VideoContext::Get()->Reset();
 }
 
 

commit 386cc9d3cdcece746b12d434f979eeaad507cbea

Reword error messages introduced in r2998 to make it Aegisub's fault and not the graphics drivers'. Even if our code is technically correct, it's still our problem. Updates #799.
Originally committed to SVN as r2999.

aegisub/src/video_context.cpp
@@ -438,7 +438,7 @@ void VideoContext::JumpToFrame(int n) {
 		catch (const wxChar *err) {
 			wxLogError(
 				_T("Failed seeking video. The video will be closed because of this.\n")
-				_T("If you get this error regardless of which video file you use, and also if you use dummy video, your OpenGL driver might not work with Aegisub.\n")
+				_T("If you get this error regardless of which video file you use, and also if you use dummy video, Aegisub might not work with your graphics card's OpenGL driver.\n")
 				_T("Error message reported: %s"),
 				err);
 			Reset();
@@ -446,7 +446,7 @@ void VideoContext::JumpToFrame(int n) {
 		catch (...) {
 			wxLogError(
 				_T("Failed seeking video. The video will be closed because of this.\n")
-				_T("If you get this error regardless of which video file you use, and also if you use dummy video, your OpenGL driver might not work with Aegisub.\n")
+				_T("If you get this error regardless of which video file you use, and also if you use dummy video, Aegisub might not work with your graphics card's OpenGL driver.\n")
 				_T("No further error message given."));
 			Reset();
 		}

aegisub/src/video_display.cpp
@@ -313,7 +313,7 @@ try {
 catch (const wxChar *err) {
 	wxLogError(
 		_T("An error occurred trying to render the video frame to screen.\n")
-		_T("If you get this error regardless of which video file you use, and also if you use dummy video, your OpenGL driver might not work with Aegisub.\n")
+		_T("If you get this error regardless of which video file you use, and also if you use dummy video, Aegisub might not work with your graphics card's OpenGL driver.\n")
 		_T("Error message reported: %s"),
 		err);
 	VideoContext::Get()->Reset();
@@ -321,7 +321,7 @@ catch (const wxChar *err) {
 catch (...) {
 	wxLogError(
 		_T("An error occurred trying to render the video frame to screen.\n")
-		_T("If you get this error regardless of which video file you use, and also if you use dummy video, your OpenGL driver might not work with Aegisub.\n")
+		_T("If you get this error regardless of which video file you use, and also if you use dummy video, Aegisub might not work with your graphics card's OpenGL driver.\n")
 		_T("No further error message given."));
 	VideoContext::Get()->Reset();
 }