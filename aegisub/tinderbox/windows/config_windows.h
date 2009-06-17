e20204610fb951dd998ca424041e897c6800520a
Niels Martin Hansen <nielsm@aegisub.org>
2009-06-17 03:57:03 +0000

    New config for Windows buildbot.  * Add ffms2 support to build  * Make sure Avisynth and ffms2 support is only built in 32 bit builds, as they can't work in 64 bit yet.
    Originally committed to SVN as r3075.

--- aegisub/tinderbox/windows/config_windows.h
+++ aegisub/tinderbox/windows/config_windows.h
@@ -43,135 +43,45 @@
 #endif
 
 
-// Endianness: We don't support any Windows version that runs on big endian
 #define HAVE_LITTLE_ENDIAN
 #undef HAVE_BIG_ENDIAN
 
 
-////////////// HIGH PRIORITY /////////////
-
-// Enable Automation
-// Requires: Nothing
 #define WITH_AUTOMATION
-
-
-// Enable Automation 4 Lua
-// Requires: Lua 5.1 (in repository)
 #define WITH_AUTO4_LUA
-
-
-// Enable Automation 3
-// Requires: auto3 dll (in repository), Lua (in repository)
 #define WITH_AUTO3
-
-
-// Enable DirectSound audio player
-// Requires: DirectX SDK
-#define WITH_DIRECTSOUND
-
-
-// Enable Avisynth
-// Requires: nothing (just the avisynth dlls)
-#define WITH_AVISYNTH
-
-
-
-///////////// MEDIUM PRIORITY ////////////
-
-// Enable FreeType2 font lister for the fonts collector
-// If you're on Visual Studio, also uncomment the library names and make sure they match the files that you have
-// Requires: FreeType2
-#define WITH_FREETYPE2
-#define FT2_LIB_RELEASE "freetype239.lib"
-#define FT2_LIB_DEBUG "freetype239_D.lib"
-
-
-// Enable CSRI, required for styles previews in the style editor and some video providers
-// Requires: csri (in repository)
-#define WITH_CSRI
-
-
-// Enable universal charset detector, so Aegisub can automatically detect the encoding of non-unicode subtitles
-// Requires: universalcharset (in repository)
-#define WITH_UNIVCHARDET
-
-
-// Enable Hunspell-based spellchecker
-// Requires: hunspell (in repository for Win32)
-// If you have an old version of Hunspell (that uses Hunspell::put_word() instead of Hunspell::add()),
-// uncomment the second line as well.
-#define WITH_HUNSPELL
-//#define WITH_OLD_HUNSPELL
-
-
-
-/////////////// LOW PRIORITY ////////////
-
-// Enable DirectShow video provider
-// Requires: DirectShow "baseclasses", DirectX SDK
-//#define WITH_DIRECTSHOW
-
-
-// Enable Perl scripting
-// Requires: perl library (ActivePerl comes with one for Visual C++ under lib\core\)
 //#define WITH_PERL
-
-// Enable PerlConsole (a debug tool for the perl engine)
-// You don't want it
 //#define WITH_PERLCONSOLE
-
-
-
-///////////// NOT RECOMMENDED /////////////
-
-// Enable FontConfig
-// Requires: fontconfig
-//#define WITH_FONTCONFIG
-
-
-// Enable libass
-// Requires: libass
-//#define WITH_LIBASS
-
-
-// Enable FFmpeg video and audio decoders
-// Requires: libavcodec, libavformat, libswscale, libavutil
-// If you compiled static libraries (yes, by default), uncomment the second line as well,
-// and remember to add the correct .a files to the linker's additional dependencies.
-// #define WITH_FFMPEG
-// #define WITH_STATIC_FFMPEG
-
-
-// Enable ffmpegsource video and audio providers
-// Requires: ffmpegsource version 2
-//#define WITH_FFMPEGSOURCE
-
-
-// Enable Ruby support for Automation
-// Requires: Ruby 1.9
 //#define WITH_RUBY
 
 
-// Enable PortAudio audio player
-// Requires PortAudio release 18
+#define WITH_DIRECTSOUND
 //#define WITH_PORTAUDIO
-
-
-// Enable PortAudio audio player version 2
-// Requires PortAudio release 19
-//#define WITH_PORTAUDIO2
-
-
-// Enable ALSA audio player
-// Requires Linux and libasound
 //#define WITH_ALSA
-
-
-// Enable OpenAL audio player
-// Requires OpenAL development libraries and headers
 //#define WITH_OPENAL
-
-
-// Enable Pulse Audio audio player
-// Requires libpulse (and a *NIX compatible system and a running sound server to actually use)
 //#define WITH_PULSEAUDIO
+
+
+#if !defined(_M_X64) && !defined(_M_IA64)
+# define WITH_AVISYNTH
+# define WITH_FFMPEGSOURCE
+#endif
+//#define WITH_DIRECTSHOW
+//#define WITH_FFMPEG
+//#define WITH_STATIC_FFMPEG
+
+
+#define WITH_FREETYPE2
+#define FT2_LIB_RELEASE "freetype239.lib"
+#define FT2_LIB_DEBUG "freetype239_D.lib"
+//#define WITH_FONTCONFIG
+
+#define WITH_UNIVCHARDET
+
+#define WITH_CSRI
+//#define WITH_LIBASS
+
+#define WITH_HUNSPELL
+
+//#define WITH_STARTUPLOG
+//#define FINAL_RELEASE
