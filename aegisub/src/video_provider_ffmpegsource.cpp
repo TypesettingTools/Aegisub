5cb4d1cae3415ccfc1e3327f1eecf500f52f80b4
Karl Blomster <kalle.blomster@gmail.com>
2009-07-15 04:02:15 +0000

    Revert r3137 for video_provider_ffmpegsource.cpp; the patch fixed an issue that had already been fixed (by using UTF8 instead of the local codepage).
    Originally committed to SVN as r3144.

--- aegisub/src/video_provider_ffmpegsource.cpp
+++ aegisub/src/video_provider_ffmpegsource.cpp
@@ -45,7 +45,6 @@
 #include "video_context.h"
 #include "options.h"
 #include "aegisub_endian.h"
-#include "charset_conv.h"
 #ifdef WIN32
 #include <objbase.h>
 #endif
@@ -168,7 +167,7 @@ void FFmpegSourceVideoProvider::LoadVideo(Aegisub::String filename, double fps) 
 		throw ErrorMsg;
 	}
 
-	VideoSource = FFMS_CreateVideoSource(FileNameWX.mb_str(csConvLocal), TrackNumber, Index, "", Threads, SeekMode, FFMSErrorMessage, MessageSize);
+	VideoSource = FFMS_CreateVideoSource(FileNameWX.mb_str(wxConvUTF8), TrackNumber, Index, "", Threads, SeekMode, FFMSErrorMessage, MessageSize);
 	FFMS_DestroyIndex(Index);
 	Index = NULL;
 	if (VideoSource == NULL) {
