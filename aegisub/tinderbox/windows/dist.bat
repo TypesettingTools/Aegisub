b7412a08c62016da444a6668e1e78554b3e47067
Thomas Goyne <plorkyeran@aegisub.org>
2009-07-24 00:38:48 +0000

    Make the windows buildbot not try to copy the now non-existent auto3 dll.
    Originally committed to SVN as r3245.

--- aegisub/tinderbox/windows/dist.bat
+++ aegisub/tinderbox/windows/dist.bat
@@ -2,7 +2,5 @@ set dist_dir="aegisub-%1-%2-%3-r%4"
 md %dist_dir%
 xcopy aegisub\tinderbox\windows\bin\aegisub??.exe %dist_dir%
 xcopy aegisub\tinderbox\windows\bin\aegisub??.pdb %dist_dir%
-xcopy aegisub\tinderbox\windows\bin\libauto3_??.dll %dist_dir%
-xcopy aegisub\tinderbox\windows\bin\libauto3_??.pdb %dist_dir%
 
 "c:\Program Files (x86)\7-Zip\7z.exe" -bd a dist.7z %dist_dir%/*
