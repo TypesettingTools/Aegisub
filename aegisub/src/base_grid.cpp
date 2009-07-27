TypesettingTools/Aegisub

commit 5ac44f5c76b95152e783110542980714ae0d1b20

Fix #953, somewhat hackish: The grid never sized down because the scroll bar was a child window of it, and when a window has child windows, the bottom and rightmost coordinates of those together determine the minimum size of the containing window. This behaviour changes if those child windows are managed by a sizer. By putting the scroll bar into a sizer the problem goes away. I think a better solution would be to change to use wxWindow::SetScrollbar() but that's a larger operation.
git-svn-id: svn+ssh://aegisub/var/svn/aegisub/trunk@3295 93549f3f-7f0a-0410-a4b3-e966c9c94f04

aegisub/src/base_grid.cpp
@@ -38,6 +38,7 @@
 // Includes
 #include "config.h"
 
+#include <wx/sizer.h>
 #include "base_grid.h"
 #include "utils.h"
 #include "ass_file.h"
@@ -69,6 +70,12 @@ BaseGrid::BaseGrid(wxWindow* parent, wxWindowID id, const wxPoint& pos, const wx
 	// Set scrollbar
 	scrollBar = new wxScrollBar(this,GRID_SCROLLBAR,wxDefaultPosition,wxDefaultSize,wxSB_VERTICAL);
 	scrollBar->SetScrollbar(0,10,100,10);
+
+	wxBoxSizer *scrollbarpositioner = new wxBoxSizer(wxHORIZONTAL);
+	scrollbarpositioner->AddStretchSpacer();
+	scrollbarpositioner->Add(scrollBar, 0, wxEXPAND, 0);
+
+	SetSizerAndFit(scrollbarpositioner);
 
 	// Set style
 	UpdateStyle();
@@ -769,6 +776,8 @@ void BaseGrid::AdjustScrollbar() {
 /////////////////////
 // Set column widths
 void BaseGrid::SetColumnWidths() {
+	if (!IsShownOnScreen()) return;
+
 	// Width/height
 	int w = 0;
 	int h = 0;

commit 28e974e6653a836aa5a6e77a90596a6e1b63f4f4

Fix crash (div by 0) introduced in r3295, updates #953. (Blind fix, doesn't happen on Windows.)
Originally committed to SVN as r3297.

aegisub/src/base_grid.cpp
@@ -66,6 +66,7 @@ BaseGrid::BaseGrid(wxWindow* parent, wxWindowID id, const wxPoint& pos, const wx
 	bmp = NULL;
 	holding = false;
 	byFrame = false;
+	lineHeight = 1; // non-zero to avoid div by 0
 
 	// Set scrollbar
 	scrollBar = new wxScrollBar(this,GRID_SCROLLBAR,wxDefaultPosition,wxDefaultSize,wxSB_VERTICAL);