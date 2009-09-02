eceb72d95eeb4b92e39d46dcb1b9a6442e1adfde
Amar Takhar <verm@aegisub.org>
2009-09-02 08:10:59 +0000

    Add (const char*) to silence warning to strcmp.
    Originally committed to SVN as r3454.

--- aegisub/src/font_file_lister_fontconfig.cpp
+++ aegisub/src/font_file_lister_fontconfig.cpp
@@ -73,7 +73,7 @@ wxArrayString FontConfigFontFileLister::DoGetFilesWithFace(wxString facename) {
 	if (!final) return results;
 	if (FcPatternGetString(final, FC_FILE, 0, &filename) == FcResultMatch && FcPatternGetInteger(final, FC_INDEX, 0, &fontindex) == FcResultMatch) {
 		FcPatternGetString(final, FC_FAMILY, fontindex, &gotfamily);
-		if (strcmp(gotfamily,buffer) == 0) {
+		if (strcmp((const char*)gotfamily,buffer) == 0) {
 			results.Add(wxString((char*) filename,csConvLocal));
 		}
 	}
