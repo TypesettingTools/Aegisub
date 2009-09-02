1af100f284760480f90b5edc2bd61d0af1d6ea1f
Amar Takhar <verm@aegisub.org>
2009-09-02 05:01:54 +0000

    Supplant ${HOME} with /home/verm, I forgot that the buildslaves run with an extremely limited env.  This isn't a perm solution but it'll get us building for now.
    Originally committed to SVN as r3450.

--- aegisub/tinderbox/unix/dist.sh
+++ aegisub/tinderbox/unix/dist.sh
@@ -15,7 +15,7 @@ else
   BIN_MAKE="make"
 fi
 
-ACLOCAL_FLAGS="-I ${HOME}/build/wx/share/aclocal" ./autogen.sh ${CONFIGURE_ARGS} || exit $?
+ACLOCAL_FLAGS="-I /home/verm/build/wx/share/aclocal" ./autogen.sh ${CONFIGURE_ARGS} || exit $?
 
 # XXX: Work around ancient versions of intltool (cough DEBIAN)
 touch intltool-update.in intltool-merge.in intltool-extract.in
