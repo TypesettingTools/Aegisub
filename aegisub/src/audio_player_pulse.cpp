430286cf691895a177b4e634d7aa152a8a0aafe5
Amar Takhar <verm@aegisub.org>
2009-09-02 09:00:03 +0000

    Silence warnings for the pulseaudio player.
    Originally committed to SVN as r3465.

--- aegisub/src/audio_player_pulse.cpp
+++ aegisub/src/audio_player_pulse.cpp
@@ -106,7 +106,7 @@ void PulseAudioPlayer::OpenStream()
 
 	// Connect the context
 	//printf("Connecting context\n");
-	pa_context_connect(context, NULL, 0, NULL);
+	pa_context_connect(context, NULL, PA_CONTEXT_NOAUTOSPAWN, NULL);
 	// Wait for connection
 	while (true) {
 		context_notify.Wait();
@@ -149,7 +149,7 @@ void PulseAudioPlayer::OpenStream()
 
 	// Connect stream
 	//printf("Connecting playback stream\n");
-	paerror = pa_stream_connect_playback(stream, NULL, NULL, PA_STREAM_INTERPOLATE_TIMING|PA_STREAM_NOT_MONOTONOUS|PA_STREAM_AUTO_TIMING_UPDATE, NULL, NULL);
+	paerror = pa_stream_connect_playback(stream, NULL, NULL, (pa_stream_flags_t)(PA_STREAM_INTERPOLATE_TIMING|PA_STREAM_NOT_MONOTONOUS|PA_STREAM_AUTO_TIMING_UPDATE), NULL, NULL);
 	if (paerror) {
 		printf("PulseAudio reported error: %s (%d)\n", pa_strerror(paerror), paerror);
 		wxString s(pa_strerror(paerror), wxConvUTF8);
@@ -232,7 +232,7 @@ void PulseAudioPlayer::Play(int64_t start,int64_t count)
 
 	play_start_time = 0;
 	pa_threaded_mainloop_lock(mainloop);
-	paerror = pa_stream_get_time(stream, &play_start_time);
+	paerror = pa_stream_get_time(stream, (pa_usec_t*) &play_start_time);
 	pa_threaded_mainloop_unlock(mainloop);
 	if (paerror) {
 		printf("PulseAudio player: Error getting stream time: %s (%d)\n", pa_strerror(paerror), paerror);
