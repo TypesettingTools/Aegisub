eaf3353982319544c8930224f1cad4da613442c6
Amar Takhar <verm@aegisub.org>
2009-09-27 01:53:42 +0000

    Fix location of config.dat on OSX.
    Originally committed to SVN as r3588.

--- aegisub/reporter/aegisub.cpp
+++ aegisub/reporter/aegisub.cpp
@@ -30,10 +30,10 @@
 Aegisub::Aegisub() {
 	wxStandardPathsBase &paths = wxStandardPaths::Get();
 // Using ifdefs is a pain but it's much easier to centralise this.
-#ifdef __UNIX__
+#if defined(__APPLE__)
+	wxString configdir =  wxString::Format("%s-%s", paths.GetUserDataDir(), _T(AEGISUB_VERSION_DATA));
+#elif defined(__UNIX__)
 	wxString configdir =  wxString::Format("%s/.aegisub-%s", paths.GetUserConfigDir(), _T(AEGISUB_VERSION_DATA));
-#elif __APPLE__
-	wxString configdir =  wxString::Format("%s/Aegisub-%s", paths.GetUserConfigDir(), _T(AEGISUB_VERSION_DATA));
 #else
 	wxString configdir =  wxString::Format("%s/Aegisub", paths.GetUserConfigDir());
 #endif
