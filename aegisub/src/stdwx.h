1b2efdc51ef0b5a06945562f56554c3ec6a7b259
Amar Takhar <verm@aegisub.org>
2009-10-31 02:23:30 +0000

    Do a verbatium commit of agi_pre.h from trunk to stdwx.h.  This should give us the same speed increase we've seen in trunk.
    Originally committed to SVN as r3752.

--- aegisub/src/stdwx.h
+++ aegisub/src/stdwx.h
@@ -25,62 +25,209 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 //
-// -----------------------------------------------------------------------------
+// Aegisub Project http://www.aegisub.org/
 //
-// AEGISUB
-//
-// Website: http://aegisub.cellosoft.com
-// Contact: mailto:zeratul@cellosoft.com
-//
+// $Id: agi_pre.h 3722 2009-10-18 18:57:01Z verm $
 
+/// @file agi_pre.h
+/// @brief Precompiled headers include file, including all headers that should be precompiled
+/// @ingroup main
+///
+/// In order to use it, set the project to use this header as precompiled and
+/// insert it in every source file (under C/C++ -> Advanced -> Force Includes),
+/// then set stdwx.cpp to generate the precompiled header
+///
+/// @note Make sure that you disable use of precompiled headers on md5.c and
+///       MatroskaParser.c, as well as any possible future .c files.
 
-//
-// Precompiled Header File
-//
-// In order to use it, set the project to use this header as precompiled and
-// insert it in every source file (under C/C++ -> Advanced -> Force Includes),
-// then set stdwx.cpp to generate the precompiled header
-//
-// Note: make sure that you disable use of precompiled headers on md5.c and
-// MatroskaParser.c, as well as any possible future .c files.
-//
+#ifndef AGI_PRE_H
 
+/// @brief Inclusion guard.
+/// @todo Why is this even nessicary? GCC seems to include agi_pre.h twice for no reason.
+#define AGI_PRE_H
 
-////////////
 // C++ only
 #ifdef __cplusplus
 
-/////////
-// Setup
-#define WIN32_LEAN_AND_MEAN
-
-
 #include "config.h"
 
+/////////
+// Setup
+#define AGI_PRE
+
+// Block msvc from complaining about not using msvc-specific versions for
+// insecure C functions.
+#ifdef _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS_DEFINED
+#else
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
+///////////////
+// STD headers
+#include <algorithm>
+#include <deque>
+#include <fstream>
+#include <iostream>
+#include <list>
+#include <map>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+// General headers
+#include <assert.h>
+#include <ctype.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <iconv.h>
+#include <locale.h>
+#include <math.h>
+#ifdef _OPENMP
+// Not all compilers have <omp.h> (example: MSVC Express)
+#include <omp.h>
+#endif
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <wchar.h>
+
+#ifdef _WIN32
+// "Lean and mean" causes windows.h to include less stuff, mostly rarely-used things.
+// We can't build without being "lean and mean", some of the things included by it has
+// macros that clash with variable names around Aegisub causing strange build errors.
+#define WIN32_LEAN_AND_MEAN
+// Windows.h must always be the first one, it defines a load of important things
+#include <windows.h>
+#include <objbase.h>
+#include <mmsystem.h>
+//#include <process.h> // Currently only used in audio_player_dsound2.cpp
+
+#else
+
+#include <sys/fcntl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>
+#include <sys/param.h>
+#include <unistd.h>
+#endif
 
 /////////////////////
 // wxWidgets headers
-#include <wx/wxprec.h>
-#include <wx/notebook.h>
-#include <wx/statline.h>
-#include <wx/tglbtn.h>
-#include <wx/tokenzr.h>
-#include <wx/wfstream.h>
-#include <wx/filename.h>
-#include <wx/sashwin.h>
-#include <wx/file.h>
-#include <wx/filedlg.h>
-#include <wx/grid.h>
-#include <wx/fontdlg.h>
+#include <wx/wxprec.h> // Leave this first.
+
+// Windows
+#ifdef __WXMSW__
+#include <wx/msw/registry.h>
+#endif
+
+// All platforms.
+#include <wx/accel.h>
+#include <wx/app.h>
+#include <wx/arrstr.h>
+#include <wx/bitmap.h>
+#include <wx/bmpbuttn.h>
+#include <wx/button.h>
+#include <wx/checkbox.h>
+#include <wx/checklst.h>
+#include <wx/choicdlg.h>
+#include <wx/choice.h>
+#include <wx/choicebk.h>
 #include <wx/clipbrd.h>
-#include <wx/msgdlg.h>
-#include <wx/stackwalk.h>
-#include <wx/spinctrl.h>
-#include <wx/wfstream.h>
-#include <wx/tipdlg.h>
+#include <wx/colordlg.h>
+#include <wx/colour.h>
+#include <wx/combobox.h>
+#include <wx/config.h>
+#include <wx/control.h>
+#include <wx/dataobj.h>
+#include <wx/datetime.h>
+#include <wx/dc.h>
+#include <wx/dcclient.h>
+#include <wx/dcmemory.h>
+#include <wx/dcscreen.h>
+#include <wx/dialog.h>
+#include <wx/dir.h>
+#include <wx/dirdlg.h>
+#include <wx/display.h>
+#include <wx/dnd.h>
+#include <wx/docview.h>
+#include <wx/dynarray.h>
 #include <wx/event.h>
+#include <wx/file.h>
+#include <wx/fileconf.h>
+#include <wx/filedlg.h>
+#include <wx/filefn.h>
+#include <wx/filename.h>
+#include <wx/filesys.h>
+#include <wx/font.h>
+#include <wx/fontdlg.h>
+#include <wx/fontenum.h>
+#include <wx/frame.h>
+#include <wx/fs_inet.h>
+#include <wx/gauge.h>
+#include <wx/gbsizer.h>
+#include <wx/gdicmn.h>
+#include <wx/glcanvas.h>
+#include <wx/grid.h>
+#include <wx/hashmap.h>
+#include <wx/icon.h>
+#include <wx/image.h>
+#include <wx/intl.h>
+#include <wx/laywin.h>
+#include <wx/listbox.h>
+#include <wx/listctrl.h>
+#include <wx/log.h>
+#include <wx/menu.h>
+#include <wx/menuitem.h>
+#include <wx/mimetype.h>
+#include <wx/msgdlg.h>
+#include <wx/mstream.h>
+#include <wx/notebook.h>
+#include <wx/panel.h>
+#include <wx/radiobox.h>
+#include <wx/radiobut.h>
+#include <wx/rawbmp.h>
+#include <wx/recguard.h>
+#include <wx/regex.h>
+#include <wx/sashwin.h>
+#include <wx/scrolbar.h>
+#include <wx/settings.h>
+#include <wx/sizer.h>
+#include <wx/slider.h>
+#include <wx/spinctrl.h>
+#include <wx/stackwalk.h>
+#include <wx/statbmp.h>
+#include <wx/statbox.h>
+#include <wx/statline.h>
+#include <wx/stattext.h>
 #include <wx/stc/stc.h>
+#include <wx/stdpaths.h>
+#include <wx/stopwatch.h>
+#include <wx/strconv.h>
 #include <wx/string.h>
+#include <wx/sysopt.h>
+#include <wx/textctrl.h>
+#include <wx/textfile.h>
+#include <wx/tglbtn.h>
+#include <wx/thread.h>
+#include <wx/timer.h>
+#include <wx/tipdlg.h>
+#include <wx/tokenzr.h>
+#include <wx/toolbar.h>
+#include <wx/treebook.h>
+#include <wx/txtstrm.h>
+#include <wx/utils.h>
+#include <wx/validate.h>
+#include <wx/valtext.h>
+#include <wx/wfstream.h>
+#include <wx/window.h>
+#include <wx/xml/xml.h>
+#include <wx/zipstrm.h>
+
 #ifdef HAVE_APPLE_OPENGL_FRAMEWORK
 #include <OpenGL/GL.h>
 #include <OpenGL/glu.h>
@@ -88,16 +235,11 @@
 #include <GL/gl.h>
 #include <GL/glu.h>
 #endif
-#include <wx/glcanvas.h>
 
-
-
-///////////////
-// STD headers
-#include <vector>
-#include <list>
-#include <map>
-
-
+#ifndef _CRT_SECURE_NO_WARNINGS
+#undef _CRT_SECURE_NO_WARNINGS
+#endif
 
 #endif // C++
+
+#endif // AGI_PRE_H
