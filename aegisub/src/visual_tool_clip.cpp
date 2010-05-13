1654f4a1029ca7ed59f9b4852073e1f5006f4743
Thomas Goyne <plorkyeran@aegisub.org>
2010-05-13 18:37:51 +0000

    Remove a redundant (and incorrect) check for if the mouse is over the video
    Originally committed to SVN as r4279.

--- aegisub/src/visual_tool_clip.cpp
+++ aegisub/src/visual_tool_clip.cpp
@@ -80,7 +80,6 @@ void VisualToolClip::Update() {
 /// @return 
 ///
 void VisualToolClip::Draw() {
-	if (mouseX == INT_MIN || mouseY == INT_MIN) return;
 	// Get current line
 	AssDialogue *line = GetActiveDialogueLine();
 	if (!line) return;
