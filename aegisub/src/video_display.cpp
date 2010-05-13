290353d467e9b3c423f1f603be95af76f4f19575
Thomas Goyne <plorkyeran@aegisub.org>
2010-05-13 18:37:41 +0000

    Bump copyright years on video_display.*
    Originally committed to SVN as r4277.

--- aegisub/src/video_display.cpp
+++ aegisub/src/video_display.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2005-2007, Rodrigo Braz Monteiro
+// Copyright (c) 2005-2010, Rodrigo Braz Monteiro
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
