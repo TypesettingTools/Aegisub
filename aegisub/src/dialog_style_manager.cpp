767a05484f507d10e3e0515b9019cca60ce375db
Thomas Goyne <plorkyeran@aegisub.org>
2010-05-17 19:50:36 +0000

    Merge r4303 from trunk: Make all of the controls in the Style Manager dynamically sized. Results in slightly wider buttons with English and much better sized buttons in other languages. Closes #1101.
    Originally committed to SVN as r4304.

--- aegisub/src/dialog_style_manager.cpp
+++ aegisub/src/dialog_style_manager.cpp
@@ -68,8 +68,8 @@ DialogStyleManager::DialogStyleManager (wxWindow *parent,SubtitlesGrid *_grid)
 	// Catalog
 	wxSizer *CatalogBox = new wxStaticBoxSizer(wxHORIZONTAL,this,_("Catalog of available storages"));
 	CatalogList = new wxComboBox(this,LIST_CATALOG, _T(""), wxDefaultPosition, wxSize(-1,-1), 0, NULL, wxCB_READONLY | wxCB_READONLY, wxDefaultValidator, _T("Catalog List"));
-	wxButton *CatalogNew = new wxButton(this, BUTTON_CATALOG_NEW, _("New"), wxDefaultPosition, wxSize(-1,-1));
-	wxButton *CatalogDelete = new wxButton(this, BUTTON_CATALOG_DELETE, _("Delete"), wxDefaultPosition, wxSize(-1,-1));
+	wxButton *CatalogNew = new wxButton(this, BUTTON_CATALOG_NEW, _("New"));
+	wxButton *CatalogDelete = new wxButton(this, BUTTON_CATALOG_DELETE, _("Delete"));
 	CatalogBox->Add(CatalogList,1,wxEXPAND | wxRIGHT | wxALIGN_RIGHT,5);
 	CatalogBox->Add(CatalogNew,0,wxRIGHT,5);
 	CatalogBox->Add(CatalogDelete,0,0,0);
@@ -80,11 +80,11 @@ DialogStyleManager::DialogStyleManager (wxWindow *parent,SubtitlesGrid *_grid)
 	wxSizer *StorageButtons = new wxBoxSizer(wxHORIZONTAL);
 	wxSizer *StorageButtonsLow = new wxBoxSizer(wxVERTICAL);
 	wxSizer *StorageListSizer = new wxBoxSizer(wxHORIZONTAL);
-	MoveToLocal = new wxButton(this, BUTTON_STORAGE_COPYTO, _("Copy to current script ->"), wxDefaultPosition, wxSize(205,-1));
-	StorageNew = new wxButton(this, BUTTON_STORAGE_NEW, _("New"), wxDefaultPosition, wxSize(40,-1));
-	StorageEdit = new wxButton(this, BUTTON_STORAGE_EDIT, _("Edit"), wxDefaultPosition, wxSize(40,-1));
-	StorageCopy = new wxButton(this, BUTTON_STORAGE_COPY, _("Copy"), wxDefaultPosition, wxSize(40,-1));
-	StorageDelete = new wxButton(this, BUTTON_STORAGE_DELETE, _("Delete"), wxDefaultPosition, wxSize(40,-1));
+	MoveToLocal = new wxButton(this, BUTTON_STORAGE_COPYTO, _("Copy to current script ->"));
+	StorageNew = new wxButton(this, BUTTON_STORAGE_NEW, _("New"));
+	StorageEdit = new wxButton(this, BUTTON_STORAGE_EDIT, _("Edit"));
+	StorageCopy = new wxButton(this, BUTTON_STORAGE_COPY, _("Copy"));
+	StorageDelete = new wxButton(this, BUTTON_STORAGE_DELETE, _("Delete"));
 	StorageButtons->Add(StorageNew,1,wxEXPAND | wxRIGHT,5);
 	StorageButtons->Add(StorageEdit,1,wxEXPAND | wxRIGHT,5);
 	StorageButtons->Add(StorageCopy,1,wxEXPAND | wxRIGHT,5);
@@ -123,13 +123,13 @@ DialogStyleManager::DialogStyleManager (wxWindow *parent,SubtitlesGrid *_grid)
 	wxSizer *CurrentButtonsLow = new wxBoxSizer(wxVERTICAL);
 	wxSizer *CurrentListSizer = new wxBoxSizer(wxHORIZONTAL);
 	wxSizer *MoveImportSizer = new wxBoxSizer(wxHORIZONTAL);
-	MoveToStorage = new wxButton(this, BUTTON_CURRENT_COPYTO, _("<- Copy to storage"), wxDefaultPosition, wxSize(-1,25));
+	MoveToStorage = new wxButton(this, BUTTON_CURRENT_COPYTO, _("<- Copy to storage"));
 	MoveImportSizer->Add(MoveToStorage,1,wxEXPAND | wxRIGHT,5);
 	MoveImportSizer->Add(new wxButton(this, BUTTON_CURRENT_IMPORT, _("Import from script...")),1,wxEXPAND,0);
-	CurrentNew = new wxButton(this, BUTTON_CURRENT_NEW, _("New"), wxDefaultPosition, wxSize(40,-1));
-	CurrentEdit = new wxButton(this, BUTTON_CURRENT_EDIT, _("Edit"), wxDefaultPosition, wxSize(40,-1));
-	CurrentCopy = new wxButton(this, BUTTON_CURRENT_COPY, _("Copy"), wxDefaultPosition, wxSize(40,-1));
-	CurrentDelete = new wxButton(this, BUTTON_CURRENT_DELETE, _("Delete"), wxDefaultPosition, wxSize(40,-1));
+	CurrentNew = new wxButton(this, BUTTON_CURRENT_NEW, _("New"));
+	CurrentEdit = new wxButton(this, BUTTON_CURRENT_EDIT, _("Edit"));
+	CurrentCopy = new wxButton(this, BUTTON_CURRENT_COPY, _("Copy"));
+	CurrentDelete = new wxButton(this, BUTTON_CURRENT_DELETE, _("Delete"));
 	CurrentButtons->Add(CurrentNew,1,wxEXPAND | wxRIGHT,5);
 	CurrentButtons->Add(CurrentEdit,1,wxEXPAND | wxRIGHT,5);
 	CurrentButtons->Add(CurrentCopy,1,wxEXPAND | wxRIGHT,5);
