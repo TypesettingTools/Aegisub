cd8305987b94e360ff8b820d442d6c5940e03ca2
Amar Takhar <verm@aegisub.org>
2010-05-21 01:19:40 +0000

    Wrap io.h with _MSC_VER since it's only needed on windows.  Eventually we'll have to create our own set of defined macros for various platforms.
    Originally committed to SVN as r4332.

--- aegisub/libaegisub/lagi_pre.h
+++ aegisub/libaegisub/lagi_pre.h
@@ -1,7 +1,7 @@
 #define LAGI_PRE
 
+// Common C
 #include <errno.h>
-#include <io.h>
 #include <math.h>
 #include <stdarg.h>
 #include <stdio.h>
@@ -9,15 +9,21 @@
 #include <sys/stat.h>
 #include <time.h>
 
+// Windows C
+#ifdef _MSC_VER
+#include <io.h>
+#endif
+
+// Common C++
 #include <fstream>
 #include <iostream>
 #include <map>
 #include <sstream>
 #include <string>
 
+// Cajun
 #include "libaegisub/cajun/elements.h"
 #include "libaegisub/cajun/reader.h"
 #include "libaegisub/cajun/visitor.h"
 #include "libaegisub/cajun/writer.h"
 
-#define LAGI_PRE
