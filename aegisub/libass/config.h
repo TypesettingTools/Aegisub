fc9607cbb1a5c7ea1d4c00c466e6e0dc16d95fb7
Thomas Goyne <plorkyeran@aegisub.org>
2010-12-12 00:33:55 +0000

    Disable deprecation warnings in libass on windows
    Originally committed to SVN as r4959.

--- aegisub/libass/config.h
+++ aegisub/libass/config.h
@@ -1,4 +1,4 @@
-#ifdef _MSVC
+#ifdef _MSC_VER
 #define CONFIG_FONTCONFIG 1
 #define CONFIG_ICONV 1
 
@@ -7,4 +7,6 @@
 #define strtoll(p, e, b) _strtoi64(p, e, b)
 
 #define M_PI 3.1415926535897932384626433832795
+
+#pragma warning(disable : 4996)
 #endif
