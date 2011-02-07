78ed8e3694644ba83f971e8b3e103aa4383273d5
Amar Takhar <verm@aegisub.org>
2011-02-07 18:02:26 +0000

    Add stub path file for Windows and hand-edit vcproj to include the file.
    Originally committed to SVN as r5318.

---
+++ aegisub/libaegisub/windows/path.cpp
@@ -0,0 +1,56 @@
+// Copyright (c) 2011,  <@aegisub.org>
+//
+// Permission to use, copy, modify, and distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+// ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+// OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+//
+// $Id$
+
+/// @file path.cpp
+/// @brief Common paths.
+/// @ingroup libaegisub
+
+
+#include "config.h"
+
+#ifndef LAGI_PRE
+#include <string>
+#endif
+
+#include <libaegisub/path.h>
+
+namespace agi {
+
+const std::string Path::Data() {
+	return "";
+}
+
+const std::string Path::Doc() {
+	return "";
+}
+
+const std::string Path::User() {
+	return "";
+}
+
+const std::string Path::Locale() {
+	return "";
+}
+
+const std::string Path::Config() {
+	return "";
+}
+
+const std::string Path::Temp() {
+	return "";
+}
+
+} // namespace agi
