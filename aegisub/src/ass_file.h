7155ace1d9acae59cf40baca4db4f1b59f81b4c3
Thomas Goyne <plorkyeran@aegisub.org>
2011-08-27 07:28:04 +0000

    Fix merge error in ass_file.h
    Originally committed to SVN as r5573.

--- aegisub/src/ass_file.h
+++ aegisub/src/ass_file.h
@@ -152,7 +152,7 @@ public:
 	/// Get the value in a [Script Info] key as string.
 	wxString GetScriptInfo(const wxString key);
 	/// Set the value of a [Script Info] key. Adds it if it doesn't exist.
-	void SetScriptInfo(wxString key, wxString const& value);
+	void SetScriptInfo(wxString const& key, wxString const& value);
 	// Add a ";" comment in the [Script Info] section
 	void AddComment(const wxString comment);
 	/// @brief Add a line to the file
