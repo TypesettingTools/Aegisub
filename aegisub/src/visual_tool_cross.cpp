1a68f12dcb65b5544f6d37632c01ccefb92c7fea
Thomas Goyne <plorkyeran@aegisub.org>
2011-12-06 20:07:57 +0000

    Fix rendering errors with the cross visual tool and detached video
    Originally committed to SVN as r5977.

--- aegisub/src/visual_tool_cross.cpp
+++ aegisub/src/visual_tool_cross.cpp
@@ -70,9 +70,9 @@ void VisualToolCross::Draw() {
 	gl.SetLineColour(*wxWHITE, 1.0, 1);
 	float lines[] = {
 		0.f, mouse_pos.Y(),
-		video_res.X(), mouse_pos.Y(),
+		video_res.X() + video_pos.X() * 2, mouse_pos.Y(),
 		mouse_pos.X(), 0.f,
-		mouse_pos.X(), video_res.Y()
+		mouse_pos.X(), video_res.Y() + video_pos.Y() * 2
 	};
 	gl.DrawLines(2, lines, 4);
 	gl.ClearInvert();
