7bd7ef7cd68fba72ac12112746ec93a41e69ad62
cantabile <cantabile.desu@gmail.com>
2012-07-24 02:39:40 +0000

    Shift times: Make Enter in the time edit do the same as clicking OK
    Originally committed to SVN as r6944.

--- aegisub/src/dialog_shift_times.cpp
+++ aegisub/src/dialog_shift_times.cpp
@@ -194,6 +194,7 @@ DialogShiftTimes::DialogShiftTimes(agi::Context *context)
 
 	Bind(wxEVT_COMMAND_BUTTON_CLICKED, &DialogShiftTimes::Process, this, wxID_OK);
 	Bind(wxEVT_COMMAND_BUTTON_CLICKED, std::tr1::bind(&HelpButton::OpenPage, "Shift Times"), wxID_HELP);
+	shift_time->Bind(wxEVT_COMMAND_TEXT_ENTER, &DialogShiftTimes::Process, this);
 	history_box->Bind(wxEVT_COMMAND_LISTBOX_DOUBLECLICKED, &DialogShiftTimes::OnHistoryClick, this);
 	context->selectionController->AddSelectionListener(this);
 }
