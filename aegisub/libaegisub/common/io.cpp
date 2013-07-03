dd4953955570a45377b4b2c6d7564d8fb883c541
Thomas Goyne <plorkyeran@aegisub.org>
2013-07-02 20:05:06 -0700

    Eliminate double dot in temporary file names
    Probably didn't cause any problems, but it was slightly ugly.

--- aegisub/libaegisub/common/io.cpp
+++ aegisub/libaegisub/common/io.cpp
@@ -45,7 +45,7 @@ std::unique_ptr<std::ifstream> Open(fs::path const& file, bool binary) {
 
 Save::Save(fs::path const& file, bool binary)
 : file_name(file)
-, tmp_name(unique_path(file.parent_path()/(file.stem().string() + "_tmp_%%%%." + file.extension().string())))
+, tmp_name(unique_path(file.parent_path()/(file.stem().string() + "_tmp_%%%%" + file.extension().string())))
 {
 	LOG_D("agi/io/save/file") << file;
 	acs::CheckDirWrite(file.parent_path());
