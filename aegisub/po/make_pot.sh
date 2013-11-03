14c57aad6e4ea0aaaf61c9a8b0cad1470128a0b5
Thomas Goyne <plorkyeran@aegisub.org>
2013-11-03 07:53:25 -0800

    Update versions in make_pot.sh

--- aegisub/po/make_pot.sh
+++ aegisub/po/make_pot.sh
@@ -14,11 +14,11 @@ maybe_append() {
 
 find ../src ../src/command -name \*.cpp -o -name \*.h \
   | xgettext --files-from=- -o - --c++ -k_ -kSTR_MENU -kSTR_DISP -kSTR_HELP -kwxT \
-  | sed 's/SOME DESCRIPTIVE TITLE./Aegisub 3.0.0/' \
-  | sed 's/YEAR/2005-2012/' \
+  | sed 's/SOME DESCRIPTIVE TITLE./Aegisub 3.1.0/' \
+  | sed 's/YEAR/2005-2013/' \
   | sed "s/THE PACKAGE'S COPYRIGHT HOLDER/Rodrigo Braz Monteiro, Niels Martin Hansen, Thomas Goyne et. al./" \
   | sed 's/PACKAGE/Aegisub/' \
-  | sed 's/VERSION/3.0.0/' \
+  | sed 's/VERSION/3.1.0/' \
   | sed 's/FIRST AUTHOR <EMAIL@ADDRESS>/Niels Martin Hansen <nielsm@aegisub.org>/' \
   | sed 's/CHARSET/UTF-8/' \
   > aegisub.pot
