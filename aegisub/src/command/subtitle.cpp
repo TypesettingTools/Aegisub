0b205e801926f0edd0b5f077ca206366aeca5ad6
Thomas Goyne <plorkyeran@aegisub.org>
2014-01-03 07:41:36 -0800

    Fix typo in help text

--- aegisub/src/command/subtitle.cpp
+++ aegisub/src/command/subtitle.cpp
@@ -91,7 +91,7 @@ struct subtitle_find : public Command {
 	CMD_NAME("subtitle/find")
 	STR_MENU("&Find...")
 	STR_DISP("Find")
-	STR_HELP("Search for text the in subtitles")
+	STR_HELP("Search for text in the subtitles")
 
 	void operator()(agi::Context *c) override {
 		c->videoController->Stop();
