15280b456129fde5d806861481478721d541d875
Thomas Goyne <plorkyeran@aegisub.org>
2014-01-07 09:45:40 -0800

    Delete some old unused stuff

--- aegisub/tinderbox/windows/dist.bat
+++
@@ -1,6 +0,0 @@
-set dist_dir="aegisub-%1-%2-%3-r%4"
-md %dist_dir%
-xcopy aegisub\tinderbox\windows\bin\aegisub??.exe %dist_dir%
-xcopy aegisub\tinderbox\windows\bin\aegisub??.pdb %dist_dir%
-
-"c:\Program Files (x86)\7-Zip\7z.exe" -bd a dist.7z %dist_dir%/*
