b7b2ea2d612a0a68a8f249e4b81edbdcc299cbbc
Thomas Goyne <plorkyeran@aegisub.org>
2014-05-13 11:13:08 -0700

    Skip building wx files that don't actually define anything

--- build/wx/setup-installed.h
+++ build/wx/setup-installed.h
@@ -1,19 +1,3 @@
 #include "wx/version.h"
 #include "wx/cpp.h"
-
 #include "wx/msw/setup.h"
-
-#pragma comment(lib, "kernel32")
-#pragma comment(lib, "user32")
-#pragma comment(lib, "gdi32")
-#pragma comment(lib, "comdlg32")
-#pragma comment(lib, "winspool")
-#pragma comment(lib, "winmm")
-#pragma comment(lib, "shell32")
-#pragma comment(lib, "comctl32")
-#pragma comment(lib, "ole32")
-#pragma comment(lib, "oleaut32")
-#pragma comment(lib, "uuid")
-#pragma comment(lib, "rpcrt4")
-#pragma comment(lib, "advapi32")
-#pragma comment(lib, "wsock32")
