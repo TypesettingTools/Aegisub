556d655c0b2c9298b68a9f57bdd44ef7b1a34f19
Thomas Goyne <plorkyeran@aegisub.org>
2014-06-06 09:13:27 -0700

    Update ffmpeg, ffms2, libass

--- build/ffmpeg/configure.sh
+++ build/ffmpeg/configure.sh
@@ -1,1 +1,40 @@
-./configure $@ --disable-avfilter --disable-avresample --disable-bzlib --disable-devices --disable-doc --disable-encoders --disable-ffmpeg --disable-ffplay --disable-ffprobe --disable-ffserver --disable-filters --disable-hwaccels --disable-muxers --disable-network --disable-postproc --disable-pthreads --disable-shared --disable-swresample --enable-avresample --enable-gpl --enable-runtime-cpudetect --enable-static --enable-zlib --extra-cflags=-D_SYSCRT --extra-cflags=-wd4005 --extra-cflags=-wd4189 --toolchain=msvc
+platform='Win32'
+suffix='x86'
+if [[ $LIB =~ amd64 ]]; then
+  platform='x64'
+  suffix='x64'
+fi
+
+./configure $@ \
+  --disable-avfilter \
+  --disable-avresample \
+  --disable-bzlib \
+  --disable-decoder=atrac3p,indeo2,indeo3,indeo4,indeo5,twinvq \
+  --disable-devices \
+  --disable-doc \
+  --disable-encoders \
+  --disable-ffmpeg \
+  --disable-ffplay \
+  --disable-ffprobe \
+  --disable-ffserver \
+  --disable-filters \
+  --disable-hwaccels \
+  --disable-muxers \
+  --disable-network \
+  --disable-postproc \
+  --disable-pthreads \
+  --disable-shared \
+  --enable-gpl \
+  --enable-runtime-cpudetect \
+  --enable-static \
+  --enable-zlib \
+  --extra-cflags=-D_SYSCRT \
+  --extra-cflags=-I../../include \
+  --extra-cflags=-MD \
+  --extra-cflags=-wd4005 \
+  --extra-cflags=-wd4189 \
+  --extra-ldflags=-LIBPATH:../../lib/$platform/Release \
+  --toolchain=msvc
+
+mv config.h ../../build/ffmpeg/config-$suffix.h
+mv config.asm ../../build/ffmpeg/config-$suffix.asm
