a0bf50dc8a523ecf5677a7ca200c66e6056bd07b
Niels Martin Hansen <nielsm@indvikleren.dk>
2015-01-18 03:03:42 +0100

    Upgrade all MSBuild projects to v12 tools
    Has no real effect, it's what VS 2013 already uses and detailed
    logging reveals that the build system doesn't even recognize v4
    and assumes v12 anyway.
    Might prevent builds with outdated tools.

--- build/version.sh
+++ build/version.sh
@@ -70,7 +70,7 @@ export VERSION_SOURCE="from git"
 
 cat << EOF > build/git_version.xml
 <?xml version="1.0" encoding="utf-8"?>
-<Project ToolsVersion="4.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+<Project ToolsVersion="12.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
   <PropertyGroup>
     <GitVersionNumber>${git_revision}</GitVersionNumber>
     <GitVersionString>${git_version_str}</GitVersionString>
