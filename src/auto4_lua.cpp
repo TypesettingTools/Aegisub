79fb5cb3fed14a763b8b947adf05beee3d928b07
Thomas Goyne <plorkyeran@aegisub.org>
2015-01-25 19:22:47 -0800

    Fix crash when deleting the last dialogue line from automation

--- src/auto4_lua.cpp
+++ src/auto4_lua.cpp
@@ -820,7 +820,7 @@ namespace {
 						++prev;
 						++it;
 					}
-					if (row != prev) break;
+					if (it == c->ass->Events.end()) break;
 					new_sel.insert(&*it);
 					if (row == original_active)
 						new_active = &*it;
