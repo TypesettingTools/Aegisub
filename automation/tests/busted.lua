26361c5003fa7036889c79fc99ad5f33d6dc2986
Thomas Goyne <plorkyeran@aegisub.org>
2015-12-20 19:48:23 -0800

    Add support for using busted 2.0 to run automation tests
    Supply a definition of socket.gettime() to remove the dependency on
    LuaSocket, as installing binary deps from luarocks doesn't work with a
    custom build of luajit.

--- automation/tests/busted.lua
+++ automation/tests/busted.lua
@@ -1,1 +1,22 @@
-require 'busted.runner'({ batch = true })
+local ffi = require("ffi")
+
+ffi.cdef[[
+  typedef struct timeval {
+    long tv_sec;
+    long tv_usec;
+  } timeval;
+
+  int gettimeofday(timeval *t, void *tzp);
+]]
+
+-- busted depends on luasocket just for gettime(), so just supply a definition
+-- of that to avoid the dep
+package.loaded['socket'] = {
+  gettime = function()
+    local t = ffi.new("timeval")
+    ffi.C.gettimeofday(t, nil)
+    return tonumber(t.tv_sec) + tonumber(t.tv_usec) / 1000000.0
+  end
+}
+
+require 'busted.runner'({ batch = true, standalone = false })
