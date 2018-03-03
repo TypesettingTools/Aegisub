bc157d9bbc00860fb7019b9c869317809a350ef7
Thomas Goyne <plorkyeran@aegisub.org>
2018-03-03 12:44:31 -0800

    Also stub out the `system` lua module that newer versions of busted use

--- automation/tests/busted.lua
+++ automation/tests/busted.lua
@@ -7,16 +7,36 @@ ffi.cdef[[
   } timeval;
 
   int gettimeofday(timeval *t, void *tzp);
+
+  typedef struct timespec {
+    long tv_sec;
+    long tv_nsec;
+  } timespec;
+  int clock_gettime(int clk_id, timespec *tp);
 ]]
+
+function gettime()
+  local t = ffi.new("timeval")
+  ffi.C.gettimeofday(t, nil)
+  return tonumber(t.tv_sec) + tonumber(t.tv_usec) / 1000000.0
+end
+function monotime()
+  local ts = ffi.new("timespec")
+  ffi.C.clock_gettime(6, ts)
+  return tonumber(ts.tv_sec) + tonumber(ts.tv_nsec) / 1000000000.0
+end
+function sleep()
+end
 
 -- busted depends on luasocket just for gettime(), so just supply a definition
 -- of that to avoid the dep
 package.loaded['socket'] = {
-  gettime = function()
-    local t = ffi.new("timeval")
-    ffi.C.gettimeofday(t, nil)
-    return tonumber(t.tv_sec) + tonumber(t.tv_usec) / 1000000.0
-  end
+  gettime = gettime
+}
+package.loaded['system'] = {
+  gettime = gettime,
+  monotime = monotime,
+  sleep = sleep
 }
 
 package.loaded['term'] = {
