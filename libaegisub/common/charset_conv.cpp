698c41afef56776cd0c6c3c4e8e7b8ef2c6a50da
wangqr <wangqr@wangqr.tk>
2019-05-16 11:04:53 -0400

    Fix iconv ConversionFailure on long path

--- libaegisub/common/charset_conv.cpp
+++ libaegisub/common/charset_conv.cpp
@@ -366,7 +366,7 @@ size_t IconvWrapper::RequiredBufferSize(std::string const& str) {
 }
 
 size_t IconvWrapper::RequiredBufferSize(const char* src, size_t srcLen) {
-	char buff[512];
+	char buff[65536];
 	size_t charsWritten = 0;
 	size_t res;
 
