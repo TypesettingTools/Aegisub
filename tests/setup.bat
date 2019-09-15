dd59fc988a3dfbc248c89ae6f49868b3399bb03d
wangqr <wangqr@wangqr.tk>
2019-09-14 21:11:03 -0400

    Fix gtest setup.bat on Windows

--- tests/setup.bat
+++ tests/setup.bat
@@ -4,13 +4,13 @@ icacls data /grant:r %USERNAME%:F /T
 rd /s /q data
 mkdir data
 
-echo '' > data/file
+copy nul data\file
 mkdir data\dir
 
-echo  > data/file_access_denied
+copy nul data\file_access_denied
 icacls data\file_access_denied /deny %USERNAME%:F
 
-echo  > data/file_read_only
+copy nul data\file_read_only
 icacls data\file_read_only /deny %USERNAME%:W
 
 
@@ -24,17 +24,14 @@ echo {"Video" : ["Entry One", "Entry Two"]} > data/mru_ok.json
 
 echo {"Video" : [1, 3]} > data/mru_invalid.json
 
-echo '1234567890' > data\ten_bytes
-echo '' > data\touch_mod_time
-
-mkdir data/options
-cp options/* data/options
+< nul set /p ="1234567890" > data\ten_bytes
+copy nul data\touch_mod_time
 
 mkdir data\dir_iterator
-echo '' > data\dir_iterator\1.a
-echo '' > data\dir_iterator\2.a
-echo '' > data\dir_iterator\1.b
-echo '' > data\dir_iterator\2.b
+copy nul data\dir_iterator\1.a
+copy nul data\dir_iterator\2.a
+copy nul data\dir_iterator\1.b
+copy nul data\dir_iterator\2.b
 
 mkdir data\options
 xcopy "%~dp0\options" data\options
@@ -47,3 +44,5 @@ xcopy "%~dp0\vfr" data\vfr\in
 
 mkdir data\keyframe
 xcopy "%~dp0\keyframe" data\keyframe
+
+ping -n 2 127.0.0.1 > nul
