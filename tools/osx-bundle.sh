88bfc0220d02124862526aa65162dccabd2ae32b
Ryan Lucia <ryan@luciaonline.net>
2020-11-08 12:33:37 -0500

    meson: initial attempt to generate Aegisub.app on macOS

--- tools/osx-bundle.sh
+++ tools/osx-bundle.sh
@@ -2,22 +2,25 @@
 
 set -e
 
-PKG_DIR=Aegisub.app
-SKEL_DIR="packages/osx_bundle"
-AEGISUB_BIN="${1}"
-SRCDIR=`pwd`
-HOME_DIR=`echo ~`
-WX_PREFIX=`${2} --prefix`
-FONTCONFIG_CONF_DIR="${3}"
+SRC_DIR="${1}"
+BUILD_DIR="${2}"
+WX_PREFIX=`${3} --prefix`
+FONTCONFIG_CONF_DIR="${4}"
+DICT_DIR="${5}"
+MESON_BUILD_OSX_BUNDLE="${6}"
 
-if ! test -d packages/osx_bundle; then
-  echo
-  echo "Make sure you're in the toplevel source directory"
+if [ "${MESON_BUILD_OSX_BUNDLE}" != "TRUE" ]; then
+  echo "Project not built with \`build_osx_bundle\`"
+  echo "Please run \`meson configure -Dbuild_osx_bundle=true\` and rebuild"
   exit 1
 fi
 
+PKG_DIR="${BUILD_DIR}/Aegisub.app"
+SKEL_DIR="${SRC_DIR}/packages/osx_bundle"
+
 if test -d "${PKG_DIR}"; then
   rm -rf "${PKG_DIR}"
+  echo "Removing old Aegisub.app"
 fi
 
 echo
@@ -31,41 +34,34 @@ mkdir -v "${PKG_DIR}/Contents/SharedSupport/dictionaries"
 
 echo
 echo "---- Copying Skel Files ----"
-if ! test -f "tools/osx-bundle.sed"; then
+if ! test -f "${BUILD_DIR}/osx-bundle.sed"; then
   echo
-  echo "NOT FOUND: tools/osx-bundle.sed"
+  echo "NOT FOUND: ${BUILD_DIR}/osx-bundle.sed"
   exit 1
 fi
 
 # used by osx-bundle.sed
-find po -name *.po | sed 's/.*\/\(.*\)\.po/        <string>\1<\/string>/; s/RS/YU/' > languages
+find "${SRC_DIR}/po" -name *.po | sed 's/.*\/\(.*\)\.po/        <string>\1<\/string>/; s/RS/YU/' > "${BUILD_DIR}/languages"
 
-find ${SKEL_DIR} -type f -not -regex ".*.svn.*"
-cp ${SKEL_DIR}/Contents/Resources/*.icns "${PKG_DIR}/Contents/Resources"
-cat ${SKEL_DIR}/Contents/Info.plist | sed -f tools/osx-bundle.sed > "${PKG_DIR}/Contents/Info.plist"
+#find "${SKEL_DIR}" -type f -not -regex ".*.svn.*"
+cp -v ${SKEL_DIR}/Contents/Resources/*.icns "${PKG_DIR}/Contents/Resources"
+cat "${SKEL_DIR}/Contents/Info.plist" | sed -f "${BUILD_DIR}/osx-bundle.sed" > "${PKG_DIR}/Contents/Info.plist"
 
-rm languages
+rm "${BUILD_DIR}/languages"
 
 echo
 echo "---- Installing files ----"
-make install \
-	DESTDIR="${PKG_DIR}/Contents" \
-	P_DATA="/SharedSupport" \
-	P_DOC="/SharedSupport/doc" \
-	P_LOCALE="/Resources" \
-	P_BINDIR="/MacOS"
+CURRENT_DIR=`pwd`
+cd ${BUILD_DIR}
+ninja install
+cd ${CURRENT_DIR}
 
 echo
 echo "---- Copying dictionaries ----"
-if test -z "${DICT_DIR}"; then
-  DICT_DIR="${HOME_DIR}/dict"
-fi
-
-if test -d "${DICT_DIR}"; then
-  cp -v ${DICT_DIR}/* "${PKG_DIR}/Contents/SharedSupport/dictionaries"
+if test -f "${DICT_DIR}"; then
+  cp -v "${DICT_DIR}/*" "${PKG_DIR}/Contents/SharedSupport/dictionaries"
 else
-  echo "WARNING: Dictionaries not found, please set $$DICT_DIR to a directiory"
-  echo "         where the *.aff and *.dic files can be found"
+  echo "Specified dictionary directory ${DICT_DIR} not found!"
 fi
 
 echo
@@ -74,14 +70,15 @@ echo "---- Copying Aegisub locale files ----"
 # internal so we don't need an aegisub.mo file.
 mkdir -vp "${PKG_DIR}/Contents/Resources/en.lproj"
 
+# FIXME
 # 10.8 wants sr_YU rather than sr_RS
-mv "${PKG_DIR}/Contents/Resources/sr_RS.lproj" "${PKG_DIR}/Contents/Resources/sr_YU.lproj"
-mv "${PKG_DIR}/Contents/Resources/sr_RS@latin.lproj" "${PKG_DIR}/Contents/Resources/sr_YU@latin.lproj"
+#mv "${PKG_DIR}/Contents/Resources/sr_RS.lproj" "${PKG_DIR}/Contents/Resources/sr_YU.lproj"
+#mv "${PKG_DIR}/Contents/Resources/sr_RS@latin.lproj" "${PKG_DIR}/Contents/Resources/sr_YU@latin.lproj"
 
 echo
 echo "---- Copying WX locale files ----"
 
-for i in `ls -1 po/*.mo|sed "s|po/\(.*\).mo|\1|"`; do
+for i in `ls -1 ${SRC_DIR}/po/*.mo|sed "s|po/\(.*\).mo|\1|"`; do
   WX_MO="${WX_PREFIX}/share/locale/${i}/LC_MESSAGES/wxstd.mo"
 
   if ! test -f "${WX_MO}"; then
@@ -95,10 +92,9 @@ for i in `ls -1 po/*.mo|sed "s|po/\(.*\).mo|\1|"`; do
   fi
 done
 
+echo
+echo "---- Fixing libraries ----"
+sudo python "${SRC_DIR}/tools/osx-fix-libs.py" "${PKG_DIR}/Contents/MacOS/aegisub" || exit $?
 
 echo
-echo "---- Libraries ----"
-python tools/osx-fix-libs.py "${PKG_DIR}/Contents/MacOS/aegisub" || exit $?
-
-echo
-echo "Done Creating \"${PKG_DIR}\""
+echo "Done creating \"${PKG_DIR}\""
