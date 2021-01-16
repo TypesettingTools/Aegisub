8b8f4a39257d974ffcd91be59cc3f98cda97063f
Ryan <ryan@luciaonline.net>
2021-01-16 05:07:26 -0500

    meson: bump minimum ffms2 version to 2.22
    It's been out for 6 years, so this should be fairly safe. Also fix a build error that somehow snuck in during the merge

--- src/video_provider_ffmpegsource.cpp
+++ src/video_provider_ffmpegsource.cpp
@@ -267,7 +267,6 @@ void FFmpegSourceVideoProvider::LoadVideo(agi::fs::path const& filename, std::st
 		CS = Width > 1024 || Height >= 600 ? AGI_CS_BT709 : AGI_CS_BT470BG;
 	RealColorSpace = ColorSpace = colormatrix_description(CS, CR);
 
-#if FFMS_VERSION >= ((2 << 24) | (17 << 16) | (1 << 8) | 0)
 	if (CS != AGI_CS_RGB && CS != AGI_CS_BT470BG && ColorSpace != colormatrix && colormatrix == "TV.601") {
 		CS = AGI_CS_BT470BG;
 		ColorSpace = colormatrix_description(AGI_CS_BT470BG, CR);
