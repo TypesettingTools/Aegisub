0c03b8abdc88eb5a8498a5c3f993956452f436ac
Totto16 <tobiausgais@gmail.com>
2022-07-11 00:57:40 +0200

    fixing current_path() not returning the right path!!

--- src/project.cpp
+++ src/project.cpp
@@ -469,9 +469,16 @@ void Project::LoadList(std::vector<agi::fs::path> const& files) {
 	};
 
 	agi::fs::path audio, video, subs, timecodes, keyframes;
+	agi::fs::path currentWorkingDir = getenv("PWD") != nullptr ? getenv("PWD") : boost::filesystem::current_path(); 
 	for (auto file : files) {
-		if (file.is_relative()) file = absolute(file);
-		if (!agi::fs::FileExists(file)) continue;
+		
+		if (file.is_relative()) {
+			file = boost::filesystem::absolute(file,currentWorkingDir);
+		}
+
+		if (!agi::fs::FileExists(file)){
+			continue;
+		}
 
 		auto ext = file.extension().string();
 		boost::to_lower(ext);
