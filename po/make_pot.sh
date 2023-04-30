42d9d6dabe38a5fbf63bcc036d09c223f7df308c
Oneric <oneric@oneric.stub>
2023-04-30 18:09:48 +0200

    po/make_pot: also update PO files
    While meson already provides the aegisub-update-po target
    for this exact purpose, it insists on also running meson’s
    aegisub-pot target first, leading to missing translation strings.

--- po/make_pot.sh
+++ po/make_pot.sh
@@ -54,3 +54,9 @@ do
     | sed 's/[^=]*=\(.*\)/packages\/win_installer\/fragment_strings.iss|1|"\1"/' \
     | maybe_append
 done
+
+for lang in $(cat LINGUAS) ; do
+  # If using gettext < 0.21, run twice to avoid reversing order of old strings
+  # ref: https://savannah.gnu.org/bugs/?58778
+  msgmerge --update --backup=none --no-fuzzy-matching "$lang".po aegisub.pot
+done
