8b3f14bbc274c801b48ab05b5d8728a0ff0677fc
Totto16 <tobiausgais@gmail.com>
2023-10-29 01:55:43 +0200

    fix dialog

--- src/dialog_about.cpp
+++ src/dialog_about.cpp
@@ -48,8 +48,8 @@ void ShowAboutDialog(wxWindow *parent) {
 		translatorCredit.clear();
 
 	// Generate about string
-	wxString aboutString = wxString("Aegisub ") + GetAegisubShortVersionString() + ".\n"
-		fmt_tl("Copyright (c) 2005-%s Rodrigo Braz Monteiro, Niels Martin Hansen, Thomas Goyne et al.\n\n",GetAegisubBuildTime())+ 
+	wxString aboutString = wxString("Aegisub ") + GetAegisubShortVersionString() + ".\n" + 
+		fmt_tl("Copyright (c) 2005-%s Rodrigo Braz Monteiro, Niels Martin Hansen, Thomas Goyne et al.\n\n",GetAegisubBuildYear())+ 
 		"Programmers:\n"
 		"    Alysson Souza e Silva\n"
 		"    Amar Takhar\n"
