70d4a89d4983c565d3d7c29810565eb2c6565e90
Totto16 <tobiausgais@gmail.com>
2023-10-29 18:23:34 +0100

    small bash script fixes

--- tools/dependency-control.sh
+++ tools/dependency-control.sh
@@ -1,18 +1,18 @@
 #!/bin/env bash
 
-cd ${MESON_BUILD_ROOT}
+cd ${MESON_BUILD_ROOT} || exit 5
 
 if ! [ -d "DependencyControl" ]; then
     mkdir "DependencyControl"
 fi
 
-cd "DependencyControl"
+cd "DependencyControl" || exit 5
 
 if ! [ -d "DependencyControl" ]; then
     # git clone https://github.com/TypesettingTools/DependencyControl.git --branch sqlite
     # git clone https://github.com/Totto16/DependencyControl.git &> /dev/null
     git clone https://github.com/TypesettingTools/DependencyControl.git &>/dev/null
-    cd "DependencyControl"
+    cd "DependencyControl" || exit 5
     git checkout tags/v0.6.4-alpha &>/dev/null
     cd ..
 fi
@@ -30,7 +30,7 @@ if ! [ -d "ffi-experiments" ]; then
     git clone https://github.com/Totto16/ffi-experiments.git &>/dev/null
 fi
 
-cd "ffi-experiments"
+cd "ffi-experiments" || exit 5
 # weird but necessary, since executning in root :(,
 #TODO FIX THAT
 sudo chmod 777 -R .
