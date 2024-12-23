ad047fb5fd66a828779d85aee0a38ee4924a4dbb
arch1t3cht <arch1t3cht@gmail.com>
2024-12-23 16:31:36 +0100

    i18n: Also rename appdata to metainfo
    This was missing from 3a07376ef9b09fb93a84f289865784f8175a6a5e .

--- po/make_pot.sh
+++ po/make_pot.sh
@@ -48,8 +48,8 @@ done
 
 if which xmlstarlet >/dev/null 2>&1 && which jq >/dev/null 2>&1; then
   for i in 'name' 'summary' 'p' 'li' 'caption'; do
-    xmlstarlet sel -t -v "//$i" ../packages/desktop/aegisub.appdata.xml.in.in | jq -R .
-  done | nl -v0 -w1 -s'|' | sed -re 's/^/aegisub.appdata.xml|/' | maybe_append
+    xmlstarlet sel -t -v "//$i" ../packages/desktop/aegisub.metainfo.xml.in.in | jq -R .
+  done | nl -v0 -w1 -s'|' | sed -re 's/^/aegisub.metainfo.xml|/' | maybe_append
 fi
 
 grep '^_[A-Za-z0-9]*=.*' ../packages/win_installer/fragment_strings.iss.in | while read line
