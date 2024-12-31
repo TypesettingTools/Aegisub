1ce7c6538b47f84a322199b7e5daea55225871e0
darealshinji <djcj@gmx.de>
2024-12-31 17:08:41 +0100

    Update version.sh (#245)
    `git describe --tags` is required to get the correct version tag and the grep regex was changed to match the version pattern x.x.x. The pattern '\d.\d.\d' doesn't seem to work with GNU grep.

--- tools/version.sh
+++ tools/version.sh
@@ -41,19 +41,20 @@ osx_bundle_sed_path="${builddir}/osx-bundle.sed"
 
 last_svn_revision=6962
 last_svn_hash="16cd907fe7482cb54a7374cd28b8501f138116be"
+
 # Check if the commit exists first, it doesn't exist in shallow clones.
 if [ "$(git cat-file -t $last_svn_hash 2> /dev/null)" = "commit" ]; then
   git_revision=$(expr $last_svn_revision + $(git rev-list --count $last_svn_hash..HEAD))
 else
   git_revision=0
 fi
-git_version_str=$(git describe --exact-match 2> /dev/null)
+git_version_str=$(git describe --tags --exact-match 2> /dev/null)
 installer_version='0.0.0'
 resource_version='0, 0, 0'
 if test x$git_version_str != x; then
   git_version_str="${git_version_str##v}"
   tagged_release=1
-  if [ $(echo $git_version_str | grep '\d\.\d\.\d') ]; then
+  if [ $(echo $git_version_str | grep '^[0-9]\.[0-9]\.[0-9]$') ]; then
     installer_version=$git_version_str
     resource_version=$(echo $git_version_str | sed 's/\./, /g')
   fi
