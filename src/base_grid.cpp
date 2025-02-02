ad5df0d741830f95320e36dffb348d1051e3de3d
arch1t3cht <arch1t3cht@gmail.com>
2025-02-02 21:02:39 +0100

    Properly resize subtitle grid on DPI change

--- src/base_grid.cpp
+++ src/base_grid.cpp
@@ -124,7 +124,13 @@ BEGIN_EVENT_TABLE(BaseGrid,wxWindow)
 	EVT_KEY_DOWN(BaseGrid::OnKeyDown)
 	EVT_CHAR_HOOK(BaseGrid::OnCharHook)
 	EVT_MENU_RANGE(MENU_SHOW_COL,MENU_SHOW_COL+15,BaseGrid::OnShowColMenu)
+	EVT_DPI_CHANGED(BaseGrid::OnDPIChanged)
 END_EVENT_TABLE()
+
+void BaseGrid::OnDPIChanged(wxDPIChangedEvent &e) {
+	UpdateStyle();
+	e.Skip();
+}
 
 void BaseGrid::OnSubtitlesCommit(int type) {
 	if (type == AssFile::COMMIT_NEW || type & AssFile::COMMIT_ORDER || type & AssFile::COMMIT_DIAG_ADDREM)
