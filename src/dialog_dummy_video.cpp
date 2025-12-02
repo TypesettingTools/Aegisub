46a42da434c2ce3f1499cbff5de7cddfd5dbb5c4
arch1t3cht <arch1t3cht@gmail.com>
2025-12-02 12:07:48 +0100

    Fix indentation in dummy video resolution list

--- src/dialog_dummy_video.cpp
+++ src/dialog_dummy_video.cpp
@@ -79,7 +79,7 @@ static ResolutionShortcut resolutions[] = {
 	{wxTRANSLATE(L"1920\u00D71080 (FHD 1080p)"), 1920, 1080},
 	{wxTRANSLATE(L"2560\u00D71440 (QHD 1440p)"), 2560, 1440},
 	{wxTRANSLATE(L"3840\u00D72160 (4K UHD 2160p)"), 3840, 2160},
-    {wxTRANSLATE(L"1080\u00D71920 (FHD vertical)"), 1080, 1920},
+	{wxTRANSLATE(L"1080\u00D71920 (FHD vertical)"), 1080, 1920},
 };
 
 wxSpinCtrl *spin_ctrl(wxWindow *parent, int min, int max, int *value) {
