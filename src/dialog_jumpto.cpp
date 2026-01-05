3a3077c6620cec9a20c453a87cea8efa97861555
0tkl <118708188+0tkl@users.noreply.github.com>
2026-01-05 23:34:32 +0800

    wxDefaultSize

--- src/dialog_jumpto.cpp
+++ src/dialog_jumpto.cpp
@@ -74,7 +74,7 @@ DialogJumpTo::DialogJumpTo(agi::Context *c)
 	auto LabelTime = new wxStaticText(&d, -1, _("Time: "));
 
 	int JumpFrameSize = std::to_string(c->project->VideoProvider()->GetFrameCount() - 1).size();
-	JumpFrame = new wxTextCtrl(&d,-1,"",wxDefaultPosition,wxSize(-1,-1),wxTE_PROCESS_ENTER, IntValidator((int)jumpframe));
+	JumpFrame = new wxTextCtrl(&d,-1,"",wxDefaultPosition,wxDefaultSize,wxTE_PROCESS_ENTER, IntValidator((int)jumpframe));
 	JumpFrame->SetInitialSize(JumpFrame->GetSizeFromText(wxString(JumpFrameSize, '0')));
 	JumpFrame->SetMaxLength(JumpFrameSize);
 	JumpTime = new TimeEdit(&d, -1, c, agi::Time(c->videoController->TimeAtFrame(jumpframe)).GetAssFormatted(), wxSize(-1,-1));
