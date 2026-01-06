TypesettingTools/Aegisub

commit 22b03bcc064e6e1a97b73adef59fcf7b816b61e9

Get rid of boost::starts_with

src/ass_override.cpp
@@ -38,7 +38,6 @@
 #include <libaegisub/split.h>
 #include <libaegisub/string.h>
 
-#include <boost/algorithm/string/predicate.hpp>
 #include <boost/range/adaptor/filtered.hpp>
 #include <boost/range/adaptor/transformed.hpp>
 #include <functional>
@@ -57,8 +56,8 @@ template<> std::string AssOverrideParameter::Get<std::string>() const {
 	if (omitted) throw agi::InternalError("AssOverrideParameter::Get() called on omitted parameter");
 	if (block.get()) {
 		std::string str(block->GetText());
-		if (boost::starts_with(str, "{")) str.erase(begin(str));
-		if (boost::ends_with(str, "}")) str.erase(end(str) - 1);
+		if (str.starts_with("{")) str.erase(begin(str));
+		if (str.starts_with("}")) str.erase(end(str) - 1);
 		return str;
 	}
 	return value;
@@ -442,7 +441,7 @@ void AssOverrideTag::Clear() {
 void AssOverrideTag::SetText(const std::string &text) {
 	load_protos();
 	for (auto cur = proto.begin(); cur != proto.end(); ++cur) {
-		if (boost::starts_with(text, cur->name)) {
+		if (text.starts_with(cur->name)) {
 			Name = cur->name;
 			parse_parameters(this, text.substr(Name.size()), cur);
 			valid = true;

libaegisub/ass/karaoke.cpp
@@ -19,7 +19,6 @@
 #include <libaegisub/karaoke_matcher.h>
 #include <libaegisub/format.h>
 
-#include <boost/algorithm/string/predicate.hpp>
 #include <boost/algorithm/string/trim.hpp>
 
 namespace agi::ass {

src/ass_dialogue.cpp
@@ -85,11 +85,11 @@ public:
 
 void AssDialogue::Parse(std::string const& raw) {
 	std::string_view str = raw;
-	if (boost::starts_with(raw, "Dialogue:")) {
+	if (raw.starts_with("Dialogue:")) {
 		Comment = false;
 		str.remove_prefix(10);
 	}
-	else if (boost::starts_with(raw, "Comment:")) {
+	else if (raw.starts_with("Comment:")) {
 		Comment = true;
 		str.remove_prefix(9);
 	}

src/ass_parser.cpp
@@ -27,7 +27,6 @@
 
 #include <algorithm>
 #include <boost/algorithm/string/case_conv.hpp>
-#include <boost/algorithm/string/predicate.hpp>
 #include <boost/algorithm/string/trim.hpp>
 #include <boost/lexical_cast.hpp>
 #include <boost/regex.hpp>
@@ -84,7 +83,7 @@ public:
 			return true;
 		}
 
-		if (boost::starts_with(key, "Automation Settings ")) {
+		if (key.starts_with("Automation Settings ")) {
 			target->Properties.automation_settings[key.substr(strlen("Automation Settings"))] = value;
 			return true;
 		}
@@ -104,7 +103,7 @@ AssParser::AssParser(AssFile *target, int version)
 AssParser::~AssParser() = default;
 
 void AssParser::ParseAttachmentLine(std::string const& data) {
-	bool is_filename = boost::starts_with(data, "fontname: ") || boost::starts_with(data, "filename: ");
+	bool is_filename = data.starts_with("fontname: ") || data.starts_with("filename: ");
 
 	bool valid_data = data.size() > 0 && data.size() <= 80;
 	for (auto byte : data) {
@@ -129,13 +128,13 @@ void AssParser::ParseAttachmentLine(std::string const& data) {
 }
 
 void AssParser::ParseScriptInfoLine(std::string const& data) {
-	if (boost::starts_with(data, ";")) {
+	if (data.starts_with(";")) {
 		// Skip stupid comments added by other programs
 		// Of course, we'll add our own in place later... ;)
 		return;
 	}
 
-	if (boost::starts_with(data, "ScriptType:")) {
+	if (data.starts_with("ScriptType:")) {
 		std::string version_str = data.substr(11);
 		boost::trim(version_str);
 		boost::to_lower(version_str);
@@ -149,7 +148,7 @@ void AssParser::ParseScriptInfoLine(std::string const& data) {
 
 	// Nothing actually supports the Collisions property and malformed values
 	// crash VSFilter, so just remove it entirely
-	if (boost::starts_with(data, "Collisions:"))
+	if (data.starts_with("Collisions:"))
 		return;
 
 	size_t pos = data.find(':');
@@ -175,22 +174,22 @@ void AssParser::ParseMetadataLine(std::string const& data) {
 }
 
 void AssParser::ParseEventLine(std::string const& data) {
-	if (boost::starts_with(data, "Dialogue:") || boost::starts_with(data, "Comment:"))
+	if (data.starts_with("Dialogue:") || data.starts_with("Comment:"))
 		target->Events.push_back(*new AssDialogue(data));
 }
 
 void AssParser::ParseStyleLine(std::string const& data) {
-	if (boost::starts_with(data, "Style:"))
+	if (data.starts_with("Style:"))
 		target->Styles.push_back(*new AssStyle(data, version));
 }
 
 void AssParser::ParseFontLine(std::string const& data) {
-	if (boost::starts_with(data, "fontname: "))
+	if (data.starts_with("fontname: "))
 		attach = std::make_unique<AssAttachment>(data, AssEntryGroup::FONT);
 }
 
 void AssParser::ParseGraphicsLine(std::string const& data) {
-	if (boost::starts_with(data, "filename: "))
+	if (data.starts_with("filename: "))
 		attach = std::make_unique<AssAttachment>(data, AssEntryGroup::GRAPHIC);
 }
 

src/command/edit.cpp
@@ -819,7 +819,7 @@ struct edit_line_join_keep_first final : public validate_sel_multiple {
 static bool try_paste_lines(agi::Context *c) {
 	std::string data = GetClipboard();
 	boost::trim_left(data);
-	if (!boost::starts_with(data, "Dialogue:")) return false;
+	if (!data.starts_with("Dialogue:")) return false;
 
 	EntryList<AssDialogue> parsed;
 	boost::char_separator<char> sep("\r\n");
@@ -944,7 +944,7 @@ void expand_times(AssDialogue *src, AssDialogue *dst) {
 }
 
 bool check_start(AssDialogue *d1, AssDialogue *d2) {
-	if (boost::starts_with(d1->Text.get(), d2->Text.get())) {
+	if (d1->Text.get().starts_with(d2->Text.get())) {
 		d1->Text = trim_text(d1->Text.get().substr(d2->Text.get().size()));
 		expand_times(d1, d2);
 		return true;
@@ -953,7 +953,7 @@ bool check_start(AssDialogue *d1, AssDialogue *d2) {
 }
 
 bool check_end(AssDialogue *d1, AssDialogue *d2) {
-	if (boost::ends_with(d1->Text.get(), d2->Text.get())) {
+	if (d1->Text.get().ends_with(d2->Text.get())) {
 		d1->Text = trim_text(d1->Text.get().substr(0, d1->Text.get().size() - d2->Text.get().size()));
 		expand_times(d1, d2);
 		return true;

src/command/video.cpp
@@ -56,7 +56,6 @@
 #include <libaegisub/util.h>
 
 #include <boost/algorithm/string/classification.hpp>
-#include <boost/algorithm/string/predicate.hpp>
 #include <boost/algorithm/string/split.hpp>
 #include <wx/msgdlg.h>
 #include <wx/textdlg.h>
@@ -475,12 +474,12 @@ static void save_snapshot(agi::Context *c, bool raw, bool subsonly = false) {
 	agi::fs::path basepath;
 
 	auto videoname = c->project->VideoName();
-	bool is_dummy = boost::starts_with(videoname.string(), "?dummy");
+	bool is_dummy = videoname.string().starts_with("?dummy");
 
 	// Is it a path specifier and not an actual fixed path?
 	if (option[0] == '?') {
 		// If dummy video is loaded, we can't save to the video location
-		if (boost::starts_with(option, "?video") && is_dummy) {
+		if (option.starts_with("?video") && is_dummy) {
 			// So try the script location instead
 			option = "?script";
 		}

src/resolution_resampler.cpp
@@ -28,7 +28,6 @@
 #include <libaegisub/ycbcr_conv.h>
 
 #include <algorithm>
-#include <boost/algorithm/string/predicate.hpp>
 #include <cmath>
 #include <wx/intl.h>
 
@@ -157,7 +156,7 @@ namespace {
 	}
 
 	void resample_line(resample_state *state, AssDialogue &diag) {
-		if (diag.Comment && (boost::starts_with(diag.Effect.get(), "template") || boost::starts_with(diag.Effect.get(), "code")))
+		if (diag.Comment && (diag.Effect.get().starts_with("template") || diag.Effect.get().starts_with("code")))
 			return;
 
 		auto blocks = diag.ParseTags();

src/subtitle_format_txt.cpp
@@ -80,12 +80,12 @@ void TXTSubtitleFormat::ReadFile(AssFile *target, agi::fs::path const& filename,
 		if (value.empty() && !OPT_GET("Tool/Import/Text/Include Blank")->GetBool()) continue;
 
 		// Check if this isn't a timecodes file
-		if (boost::starts_with(value, "# timecode"))
+		if (value.starts_with("# timecode"))
 			throw SubtitleFormatParseError("File is a timecode file, cannot load as subtitles.");
 
 		// Read comment data
 		bool isComment = false;
-		if (!comment.empty() && boost::starts_with(value, comment)) {
+		if (!comment.empty() && value.starts_with(comment)) {
 			isComment = true;
 			value.erase(0, comment.size());
 		}

commit edcf8e788c6f300ad04bf4233cc4eac5d47932d8

Fix extra braces being added when modifying transform tags
This broke in 22b03bcc064e6e1a97b73adef59fcf7b816b61e9 (oops).

src/ass_override.cpp
@@ -57,7 +57,7 @@ template<> std::string AssOverrideParameter::Get<std::string>() const {
 	if (block.get()) {
 		std::string str(block->GetText());
 		if (str.starts_with("{")) str.erase(begin(str));
-		if (str.starts_with("}")) str.erase(end(str) - 1);
+		if (str.ends_with("}")) str.erase(end(str) - 1);
 		return str;
 	}
 	return value;