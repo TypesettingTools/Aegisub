4463662feb09928ade12038b95a4af2c2ef3c0aa
arch1t3cht <arch1t3cht@gmail.com>
2026-02-12 20:33:41 +0100

    Do not scale \blur and \be when resampling
    Resampling changes the PlayRes, not the LayoutRes, and \blur scales with
    the LayoutRes.
    (\be currently does not scale with *either* resolution, but libass
    maintainers promise that *if* it ever scales with any resolution it will
    scale with LayoutRes. Either way, it's not correct to scale it with
    PlayRes.)

--- src/ass_override.cpp
+++ src/ass_override.cpp
@@ -279,8 +279,8 @@ static void load_protos() {
 	proto[++i].Set("\\fe", VariableDataType::TEXT); // \fe<charset>
 	proto[++i].Set("\\ko", VariableDataType::INT, AssParameterClass::KARAOKE); // \ko<duration>
 	proto[++i].Set("\\kf", VariableDataType::INT, AssParameterClass::KARAOKE); // \kf<duration>
-	proto[++i].Set("\\be", VariableDataType::INT, AssParameterClass::ABSOLUTE_SIZE_XY); // \be<strength>
-	proto[++i].Set("\\blur", VariableDataType::FLOAT, AssParameterClass::ABSOLUTE_SIZE_XY); // \blur<strength>
+	proto[++i].Set("\\be", VariableDataType::INT); // \be<strength>
+	proto[++i].Set("\\blur", VariableDataType::FLOAT); // \blur<strength>
 	proto[++i].Set("\\fn", VariableDataType::TEXT); // \fn<name>
 	proto[++i].Set("\\fs+", VariableDataType::FLOAT); // \fs+<size>
 	proto[++i].Set("\\fs-", VariableDataType::FLOAT); // \fs-<size>
