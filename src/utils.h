6340c0a33262082f8c24ea266ca35595cbba0b35
Ryu481 <ryusaki67@icloud.com>
2026-02-25 00:52:32 +0100

    Make Mac OS Menubar Entries  For Hide and Show All Work

--- src/utils.h
+++ src/utils.h
@@ -106,6 +106,12 @@ namespace osx {
 	bool activate_top_window_other_than(wxFrame *wx);
 	// Bring all windows to the front, maintaining relative z-order
 	void bring_to_front();
+	// Hide the application
+	void hide_aegisub();
+	// Hide all other applications
+	void hide_others();
+	// Unhide all applications
+	void show_all();
 
 namespace ime {
 	/// Inject the IME helper into the given wxSTC
