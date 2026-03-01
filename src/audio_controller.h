51014c78040ad0cc693d0448ad8222115a448bb6
Filip Hejsek <filip.hejsek@gmail.com>
2026-03-01 19:56:23 +0100

    Eliminate remaining unscoped connections

--- src/audio_controller.h
+++ src/audio_controller.h
@@ -71,6 +71,8 @@ class AudioController final : public wxEvtHandler, private agi::signal::Connecti
 	/// The current timing mode, if any; owned by the audio controller
 	std::unique_ptr<AudioTimingController> timing_controller;
 
+	agi::signal::Connection timing_controller_connection;
+
 	enum PlaybackMode {
 		PM_NotPlaying,
 		PM_Range,
