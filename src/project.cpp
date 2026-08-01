2b6c80e3ebb7855d421c9cc581a5021140e2314b
arch1t3cht <arch1t3cht@gmail.com>
2026-08-01 20:20:20 +0200

    Update project properties when saving files
    Otherwise, if the file is saved in a different directory than before
    (say, using "Save as" or when loading subtitles from a video file and
    then saving them in a different directory than the video file), the
    relative paths aren't updated to reflect the new directory.
    
    The deeper issue here is that the project.cpp / subs_controller.cpp /
    ass_file.cpp split is very messy, with various state being split or
    duplicated across the three and sometimes going out of sync, but this
    fix should cover most cases.

--- src/project.cpp
+++ src/project.cpp
@@ -162,6 +162,11 @@ void Project::LoadSubtitles(agi::fs::path path, std::string encoding, bool load_
 		LoadUnloadFiles(properties);
 }
 
+void Project::SetSubtitlesFilename(agi::fs::path path) {
+	context->path->SetToken("?script", path.parent_path());
+	UpdateRelativePaths();
+}
+
 void Project::CloseSubtitles() {
 	context->subsController->Close();
 	context->path->SetToken("?script", "");
